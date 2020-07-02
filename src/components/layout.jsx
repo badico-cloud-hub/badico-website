440a8df2fc0d061d29dcf9a4fea56106a20c5278
ydrouulis <lombardi.droulis@gmail.com>
2020-07-02 12:55:19 -0300

    feat(components): add a new layout component

---
+++ src/components/layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "../components/Header/index"
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Header/>
+            <main>{children}</main>
+        </>
+        
+    )
+}
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default Layout;
