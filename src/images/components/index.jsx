b30ad03093e24cc39016ec833d4f1f6ecfec0143
ydrouulis <lombardi.droulis@gmail.com>
2020-07-02 13:15:35 -0300

    feat: add image in hero section

---
+++ src/images/components/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import * as S from "./styled"
+
+const HeroImg = () => {
+    const { img } =  useStaticQuery(
+        graphql`
+            query {
+                img: file(relativePath: {eq: "ampulheta2.png"}) {
+                    childImageSharp {
+                        fluid(maxWidth: 966, maxHeight: 803) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        `
+    )
+
+    return(
+        <S.HeroImage fluid={img.childImageSharp.fluid} alt="An image of a hourglass"/>
+    )
+}
+
+
+export { HeroImg };
---
+++ src/images/components/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import Img from "gatsby-image";
+
+export const HeroImage = styled(Img)`
+    align-self: flex-end;
+    margin: 0;
+    z-index: 2;
+`
