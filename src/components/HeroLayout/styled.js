c310b92d618dff4e9eba33272eb3708c92f033a2
ydrouulis <lombardi.droulis@gmail.com>
2020-07-03 09:20:10 -0300

    style(components/HeroLayout): add image style

--- src/components/HeroLayout/styled.js
+++ src/components/HeroLayout/styled.js
@@ -1,39 +1,104 @@
-import styled from "styled-components";
-import media from "styled-media-query";
+import styled from "styled-components"
+import media from "styled-media-query"
 
 export const HeroWrapper = styled.div`
+  height: 500px;
   display: flex;
   flex-direction: column;
   background: #02293f;
+  z-index: 1;
+  position: relative;
 
-  .imgWrapper{
+  .imgWrapper {
     width: 70%;
     align-self: flex-end;
     margin: 0;
     z-index: 2;
-    position: relative;
-    bottom: 50px;
+    position: absolute;
+    right: 0;
+    top: 250px;
   }
 
-  
+  .imgBallWrapper { 
+    position: absolute;
+    bottom: 100px;
+    z-index: 0;
+    width: 1px;
+    overflow: hidden;
+  }
+
+
 
   ${media.greaterThan("medium")`
-
+    height: 400px;
     flex-direction: row;
-    justify-content: space-around;
     padding: 0 5%;
+    overflow: hidden;
+    position: relative;
 
-    
     .imgWrapper{
       min-width: 200px;
-      width: 43%;
+      width: 60%;
       display: inline;
+      position: absolute;
+      top: 0px;
+      right: 10px;
+    }
+
+    .imgBallWrapper{
+      z-index: 1;
+      width: 20%;
+      position: absolute;
+      top: 100px;
+      right: -90px;
     }
 
     .ball{
-      position: relative;
-      bottom: 0px;
-      left: 0px;
+      width: 100%;
+      height: 100%;
+    }
+  `}
+
+  ${media.greaterThan("1024px")`
+    height: 500px;
+
+    .imgWrapper{
+        min-width: 200px;
+        width: 50%;
+        display: inline;
+        position: absolute;
+        top: 0;
+        right: 50px;
+        z-index: 2;
+    }
+
+    .imgBallWrapper{
+      z-index: 1;
+      width: 15%;
+      position: absolute;
+      top: 100px;
+      right: -80px;
+    }
+  `}
+  ${media.greaterThan("1300px")`
+    height: 650px;
+    
+    .imgWrapper{
+      min-width: 200px;
+      width: 50%;
+      display: inline;
+      position: absolute;
+      top: 0;
+      right: 50px;
+      z-index: 2;
+    }
+
+    .imgBallWrapper{
+      z-index: 1;
+      width: 15%;
+      position: absolute;
+      top: 100px;
+      right: -130px;
     }
   `}
 `
