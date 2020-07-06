04fba08196f8c9e0035829d1f7d4b4903a39e7a5
ydrouulis <lombardi.droulis@gmail.com>
2020-07-06 14:22:54 -0300

    feat(components): add new card component

---
+++ src/components/Card/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const CardWrapper = styled.div`
+  width: 41%;
+  border: 4px solid #27debf;
+  background-color: transparent;
+  border-radius: 14px;
+  padding: 0.4rem;
+
+  p {
+    font-family: GalanoGrotesque;
+    font-size: 0.7rem;
+  }
+`
