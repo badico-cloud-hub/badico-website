9c99f184c6e67a6117fc62aab6ac29efb2ba2dc5
ydrouulis <lombardi.droulis@gmail.com>
2020-07-07 16:57:11 -0300

    refac(components/card): add new div

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,10 +5,12 @@ import * as S from "./styled"
 const Card = props => {
   return (
     <S.CardWrapper>
+      <div>
         <p>
           {props.content}
           <strong>{props.bold}</strong>
         </p>
+      </div>
     </S.CardWrapper>
   )
 }
