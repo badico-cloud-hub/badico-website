d91cba29bdb3ca47328febe327b91ba96d7ad0ec
ydrouulis <lombardi.droulis@gmail.com>
2020-07-07 16:57:47 -0300

    style(components/card): style new div

--- src/components/Card/styled.js
+++ src/components/Card/styled.js
@@ -1,14 +1,100 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const CardWrapper = styled.div`
-  width: 41%;
+  width: 43%;
   border: 4px solid #27debf;
   background-color: transparent;
   border-radius: 14px;
   padding: 0.4rem;
+  position: relative;
 
-  p {
+  div {
+    width: 100%;
+    height: 100%;
+  }
+
+  div:after {
+    content: "";
+    border: solid 5px #dbf5e7;
+    border-radius: 14px;
+    background-color: transparent;
+    z-index: -1;
+    display: inline-block;
+    width: 107%;
+    height: 108%;
+    position: absolute;
+    top: 1%;
+    left: -9%;
+  }
+
+  div p {
     font-family: GalanoGrotesque;
     font-size: 0.7rem;
   }
+
+  ${media.greaterThan("450px")`
+  width: 45%;
+  height: 135px;
+  
+
+  div{
+    height: 100%;
+  }
+
+  div p{
+    font-size: 0.9rem;
+  }
+  `}
+
+  ${media.greaterThan("500px")`
+  div:after{
+    left: -6%;
+    width: 105%;
+    height: 106%;
+  }
+
+  div p{
+    font-size: 1rem;
+  }
+  `}
+
+  ${media.greaterThan("600px")`
+  height: 160px;
+
+  div:after{
+    left: -5%;
+  }
+
+  div p{
+    font-size: 1.17rem;
+  }
+  `}
+
+  ${media.greaterThan("660px")`
+   div p{
+      font-size: 1.1rem;
+    }
+  `}
+
+  ${media.greaterThan("760px")`
+    width: 40%;
+  `}
+
+  ${media.greaterThan("971px")`
+    width: 25%;
+  `}
+
+  ${media.greaterThan("1000px")`
+    width: 20%;
+
+    div p{
+      font-size: 0.9rem;
+    }
+  `}
+
+  ${media.greaterThan("1200px")`
+    width: 18%;
+    height: 170px
+  `}
 `
