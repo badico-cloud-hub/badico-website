0cddf243de496bf03574c0921b951ef6341356a6
ydrouulis <lombardi.droulis@gmail.com>
2020-07-07 16:58:52 -0300

    refac(components/cards) refactory cards components

--- src/components/Cards/styled.js
+++ src/components/Cards/styled.js
@@ -1,73 +1,49 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import media from "styled-media-query"
 
 export const CardsWrapper = styled.article`
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 2rem;
-    position: relative;
-    
-    div:nth-child(1), div:nth-child(3){
-        margin-right: 2rem;
-    }
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 2rem;
+  position: relative;
 
-    div:nth-child(3), div:nth-child(4){
-        margin-top: 1.5rem;
-    }
-    div:nth-child(1):after {
-        content: "";
-        border: solid 5px #dbf5e7;
-        border-radius: 14px;
-        color: transparent;
-        z-index: -1;
-        display: inline-block;
-        width: 120px;
-        height: 120px;
-        position: absolute;
-        top: 4px;
-        left: -6px;
-    }
-    div:nth-child(2):after {
-        content: "";
-        border: solid 5px #dbf5e7;
-        border-radius: 14px;
-        color: transparent;
-        z-index: -1;
-        display: inline-block;
-        width: 121px;
-        height: 119px;
-        position: absolute;
-        top: 4px;
-        left: 147px;
-      }
+  div:nth-child(1),
+  div:nth-child(3) {
+    margin-right: 2rem;
+  }
 
-      div:nth-child(3):after {
-        content: "";
-        border: solid 5px #dbf5e7;
-        border-radius: 14px;
-        color: transparent;
-        z-index: -1;
-        display: inline-block;
-        width: 120px;
-        height: 120px;
-        position: absolute;
-        top: 145px;
-        left: -6px;
-    }
+  div:nth-child(3),
+  div:nth-child(4) {
+    margin-top: 1.5rem;
+  }
 
-    div:nth-child(4):after {
-        content: "";
-        border: solid 5px #dbf5e7;
-        border-radius: 14px;
-        color: transparent;
-        z-index: -1;
-        display: inline-block;
-        width: 122px;
-        height: 121px;
-        position: absolute;
-        top: 145px;
-        left: 147px;
-    }
+  ${media.greaterThan("450px")`
+    margin: 1rem;
+  `}
+  ${media.greaterThan("971px")`
+  div:nth-child(2){
+    margin-right: 2rem;
 
-    
-    
+  }
+  
+  div:nth-child(3){
+    margin-top: 0;
+    margin-right: 0;
+  }
+  div:nth-child(4) {
+    margin-top: 1.5rem;
+  }
+  `}
+
+  ${media.greaterThan("1000px")`
+  div:nth-child(3){
+    margin-right: 2rem;
+  }
+
+  div:nth-child(4) {
+    margin-top: 0;
+  }
+  `}
+
 `
