d11f31a82bde2b6f88d1c872be9beb10df08ffc2
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-07 13:58:26 -0300

    feat(components): add new components icon

---
+++ src/components/Icons/index.jsx
@@ -0,0 +1,17 @@
+import {
+  Github,
+  FacebookSquare,
+  Twitter,
+  Youtube,
+  LinkedinSquare,
+} from "@styled-icons/boxicons-logos"
+
+const Icons = {
+  Github,
+  FacebookSquare,
+  Twitter,
+  Youtube,
+  LinkedinSquare,
+}
+
+export default Icons
