0c34c9a52a3ac498af031a59a6067f786745aca3
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-10 16:02:00 -0300

    chore(components/layout): add old footer

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Header from "../components/Header/index"
-import NewFooter from "../components/NewFooter"
+import Footer from "../components/Footer"
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
-      <NewFooter />
+      <Footer />
     </>
   )
 }
