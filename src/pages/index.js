80de96fa5aea61d2a1920cc442b36d0cfa8c3bab
ydrouulis <lombardi.droulis@gmail.com>
2020-07-13 13:18:23 -0300

    feat(pages): add component team on page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import Team from "../components/Team/index"
 import CraftmanshipLayout from "../components/CraftmanshipLayout/index"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,7 +12,11 @@ const IndexPage = () => (
     <SEO title="Home" />
     <HeroLayout />
     <CraftmanshipLayout />
+<<<<<<< HEAD
     <About />
+=======
+    <Team/>
+>>>>>>> feat(pages): add component team on page
   </Layout>
 )
 
