953291746da8ce27517810dc6bbdb1dd7d987c2e
ydrouulis <lombardi.droulis@gmail.com>
2020-07-13 13:16:41 -0300

    feat(components): add new component team

---
+++ src/components/Team/TeamWrapper.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const TeamWrapper = styled.div`
+    padding: 0 2rem;
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    font-family: GalanoGrotesque;
+`
+
+export const Title = styled.h2`
+    font-weight: 500;
+    font-size: 1rem;
+    color: #02293F;
+    margin: .5rem auto;
+`
+
+export const Description = styled.p`
+    font-weight: 300;
+    font-size: .8rem;
+    padding-left: .2rem;
+    padding-right: .2rem;
+
+`
