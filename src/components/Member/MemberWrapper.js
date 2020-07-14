04f58ebc3ff0dd9a5269c9b54dd288bc6fd1cb9e
ydrouulis <lombardi.droulis@gmail.com>
2020-07-14 13:51:10 -0300

    style(components/member): add style in component

---
+++ src/components/Member/MemberWrapper.js
@@ -0,0 +1,48 @@
+import styled from "styled-components"
+
+export const MemberWrapper = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+
+    
+`
+
+export const ImgProfile = styled.img`
+    width: 63.5%;
+    border-radius: 50%;
+    align-self: center;
+`
+export const Content = styled.div`
+    padding: 0 ;
+`
+
+export const Name = styled.h1`
+    font-family: GalanoGrotesque;
+    font-weight: 500;
+    font-size: .75rem;
+    color: #02293F;
+    text-align: center;
+    margin: .5rem 0 .2rem 0;
+`
+export const PositionContainer = styled.div`
+    margin-left: 1.0rem;
+`
+
+export const Position = styled.h2`
+    color: #8CCAC4;
+    font-family: GalanoGrotesque;
+    font-weight: 500;
+    font-size: .689rem;
+    margin: 0 0 .6rem 0;
+    
+`
+
+export const Description = styled.p`
+    font-family: GalanoGrotesque;
+    color: #000000;
+    font-weight: 300;
+    font-size: .6rem;
+    text-align: left;
+    
+`
