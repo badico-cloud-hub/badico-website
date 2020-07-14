9889fb1511e264ed3ed85c9c6142468df048e1ff
ydrouulis <lombardi.droulis@gmail.com>
2020-07-14 13:53:52 -0300

    style(components/team): changing component's style

--- src/components/Team/TeamWrapper.js
+++ src/components/Team/TeamWrapper.js
@@ -3,22 +3,26 @@ import styled from "styled-components";
 export const TeamWrapper = styled.div`
     padding: 0 2rem;
     display: flex;
-    flex-wrap: wrap;
-    text-align: center;
+    flex-direction: column;
     font-family: GalanoGrotesque;
+`
+export const Container = styled.div`
+    margin-bottom: 1rem;
+
 `
 
 export const Title = styled.h2`
     font-weight: 500;
-    font-size: 1rem;
+    font-size: .88rem;
     color: #02293F;
     margin: .5rem auto;
+    text-align: center;
 `
 
 export const Description = styled.p`
     font-weight: 300;
-    font-size: .8rem;
+    font-size: .75rem;
     padding-left: .2rem;
     padding-right: .2rem;
-
+    text-align: center;
 `
