582b519fa2a10f34a81ae9f5b2fec7c8dcb9e732
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-14 11:10:30 -0300

    style:(components/typeform): change padding

--- src/components/Typeform/TypefromButton.js
+++ src/components/Typeform/TypefromButton.js
@@ -5,6 +5,7 @@ export default styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 0;
   height: 1.5rem;
   background-color: transparent;
   margin-left: 2rem;
@@ -44,60 +45,6 @@ ${media.greaterThan("huge")`
     width: 12rem;
     height: 3.5rem;
     font-size: 1.15rem;
-    padding-bottom: 0.25rem;
     margin-left: 0;
 `}
 `
-/*
-export const Contact = styled(Button)`
-display: flex;
-align-items: center;
-justify-content: center;
-height: 1.5rem;
-background-color: transparent;
-margin-left: 2rem;
-margin-top: 0.7rem;
-border-radius: 38px;
-font-size: 0.58rem;
-font-family: GalanoGrotesque;
-font-weight: 500;
-padding-bottom: 8px;
-width: 6rem;
-color: #27debf;
-border: 1px solid #27debf;
-transition: transform .2s linear;
-
-:hover {
-  transform: scaleX(1.03) scale(1.03);
-  color: #27debf;
-  background: #02293f;
-  border-color: #27debf;
-}
-
-:focus {
-  outline: #02293f;
-}
-
-${media.greaterThan("medium")`
-  width: 7rem;
-  height: 1.8rem;
-  font-size: .7rem;
-  margin-left: 0;
-`}
-
-${media.greaterThan("1024px")`
-  width: 9rem;
-  height: 2.5rem;
-  font-size: .8rem;
-  margin-left: 0;
-`}
-
-${media.greaterThan("huge")`
-  width: 12rem;
-  height: 3.5rem;
-  font-size: 1.15rem;
-  padding-bottom: 0.25rem;
-  margin-left: 0;
-`}
-`
-*/
