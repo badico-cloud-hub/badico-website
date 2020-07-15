f2dcb9ef621b9fa254f4376be3f7c55fa43e2995
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-15 12:36:33 -0300

    style(components/members): add max width

--- src/components/Members/MembersWrapper.js
+++ src/components/Members/MembersWrapper.js
@@ -1,7 +1,10 @@
 import styled from "styled-components"
 
 export const MembersWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  max-width: 1040px;
+  margin: 0 auto;
 `
