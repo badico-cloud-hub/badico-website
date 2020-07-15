2a3cde44cdcc35bf90230b0a870bd42ed84ccd42
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-15 12:35:59 -0300

    style(components/member): add media query

--- src/components/Member/MemberWrapper.js
+++ src/components/Member/MemberWrapper.js
@@ -1,48 +1,84 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const MemberWrapper = styled.div`
-    width: 50%;
-    display: flex;
-    flex-direction: column;
+  width: 50%;
+  max-width: 193px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
 
-    
+  ${media.greaterThan("1150px")`
+  width: 80%;
+  max-width: 230px;
+  `}
 `
 
 export const ImgProfile = styled.img`
-    width: 63.5%;
-    border-radius: 50%;
-    align-self: center;
+  width: 63.5%;
+  border-radius: 50%;
+  align-self: center;
+
+  ${media.greaterThan("450px")`
+  width: 65%;
+  `}
+
+  ${media.greaterThan("768px")`
+  width: 75%;
+  `}
 `
 export const Content = styled.div`
-    padding: 0 ;
+  padding: 0;
 `
 
 export const Name = styled.h1`
-    font-family: GalanoGrotesque;
-    font-weight: 500;
-    font-size: .75rem;
-    color: #02293F;
-    text-align: center;
-    margin: .5rem 0 .2rem 0;
+  font-family: GalanoGrotesque;
+  font-weight: 500;
+  font-size: 0.75rem;
+  color: #02293f;
+  text-align: center;
+  margin: 0.5rem 0 0.2rem 0;
+
+  ${media.greaterThan("450px")`
+  font-size: 0.85rem;
+  `}
+
+  ${media.greaterThan("768px")`
+  font-size: 1.2rem;
+  `}
 `
 export const PositionContainer = styled.div`
-    margin-left: 1.0rem;
+  margin-left: 1rem;
 `
 
 export const Position = styled.h2`
-    color: #8CCAC4;
-    font-family: GalanoGrotesque;
-    font-weight: 500;
-    font-size: .689rem;
-    margin: 0 0 .6rem 0;
-    
+  color: #8ccac4;
+  font-family: GalanoGrotesque;
+  font-weight: 500;
+  font-size: 0.689rem;
+  margin: 0 0 0.6rem 0;
+
+  ${media.greaterThan("450px")`
+  font-size: 0.75rem;
+  `}
+
+  ${media.greaterThan("768px")`
+  font-size: 1rem;
+  `}
 `
 
 export const Description = styled.p`
-    font-family: GalanoGrotesque;
-    color: #000000;
-    font-weight: 300;
-    font-size: .6rem;
-    text-align: left;
-    
+  font-family: GalanoGrotesque;
+  color: #000000;
+  font-weight: 300;
+  font-size: 0.6rem;
+  text-align: left;
+
+  ${media.greaterThan("450px")`
+  font-size: 0.65rem;
+  `}
+
+  ${media.greaterThan("768px")`
+  font-size: 1rem;
+  `}
 `
