c06d0251a47e86e90d6730640abe6c3dac524f68
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-16 11:34:29 -0300

    feat(gatsby-config): add new description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Badico Cloud Engineering`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `IT department Headache-free for startups. We are your IT department`,
     author: `@gatsbyjs`,
     copyright: `© 2020 Badico Cloud. All rights reserved.`,
     socialMedia: {
