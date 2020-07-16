0b5b4692436efde91277f4bc1cd2aafccf94f94f
ydrouulis <lombardi.droulis@gmail.com>
2020-07-16 17:25:59 -0300

    feat(components/craftmanship): add container

--- src/components/CraftmanshipLayout/index.jsx
+++ src/components/CraftmanshipLayout/index.jsx
@@ -1,16 +1,27 @@
 import React from "react"
 
+import Container from "../Container/index"
 import Cards from "../Cards/index"
 import CraftmanshipLayoutContent from "../CraftmanshipLayoutContent/index"
 import Wrapper from "./CraftWrapper"
 
 const CraftmanshipLayout = () => {
   return (
-  
-      <Wrapper style={{height: "100%", margin: "4rem 0", padding: "0 2rem", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+    <Wrapper
+      style={{
+        height: "100%",
+        margin: "4rem 0",
+        padding: "0 2rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Container>
         <CraftmanshipLayoutContent />
         <Cards />
-      </Wrapper>
+      </Container>
+    </Wrapper>
   )
 }
 
