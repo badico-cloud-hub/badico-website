6b26656f157f1e788a366e4173a370a7802abf98
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-16 11:48:26 -0300

    chore(components/seo): test title

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={"Badico Cloud Engineering | Your IT saveguard"}
+      title="Badico Cloud Engineering | Your IT saveguard"
       titleTemplate={`${site.siteMetadata.title}`}
       meta={[
         {
