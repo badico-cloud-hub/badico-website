43459726dc0e3a320c445f1b18f0c174c8f07d35
ydrouulis <lombardi.droulis@gmail.com>
2020-07-16 17:27:00 -0300

    feat(components/header): add container maxwidth

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
-import Logobadico from "../../images/logo.svg";
+import React from "react"
+import Logobadico from "../../images/logo.svg"
+import Container from "../Container/index"
 
 //import HeaderWrapper from "./HeaderWrapper";
 
 const Header = () => {
   return (
-    <div style={{backgroundColor: "#02293f"}}>
-      <Logobadico className="logo" alt="Badico's logo" style={{margin: "2rem 0 0 2rem", width: "150px"}}/>
+    <div style={{ backgroundColor: "#02293f" }}>
+      <Container>
+        <Logobadico
+          className="logo"
+          alt="Badico's logo"
+          style={{ margin: "2rem 0 0 2rem", width: "150px" }}
+        />
+      </Container>
     </div>
   )
 }
 
-export default Header;
+export default Header
