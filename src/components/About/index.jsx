a4b134d1f7ffe6f05cada37fd2d89846b188a88c
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-17 13:51:36 -0300

    chore(components/about): test

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,6 +5,7 @@ import * as S from "./styled"
 const About = () => {
   return (
     <S.AboutUs>
+<<<<<<< HEAD
       <Container>
         <h1>About us</h1>
         <h2>Who We are</h2>
@@ -20,6 +21,22 @@ const About = () => {
           to help you.
         </p>  
       </Container>
+=======
+      <h1>About us</h1>
+      <h2>Who We are</h2>
+      <p>
+        We specialize in web and mobile development, providing all stages of the
+        product, from the first design to the final product for users. Our
+        methodology allows us to integrate companies in a homogeneous way,
+        building and developing the company's technology department together.
+        Hello
+      </p>
+      <p>
+        If your company doesn't just want to develop na application, but build a
+        product that proudly generates knowledge and impact, we can and want to
+        help you.
+      </p>
+>>>>>>> chore(components/about): test
     </S.AboutUs>
   )
 }
