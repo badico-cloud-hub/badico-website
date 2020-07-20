f31585c874560ada5e94dbde8d76eb603093bf73
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-20 17:03:12 -0300

    feat(pages): add new components cases

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,24 @@ import HeroLayout from "../components/HeroLayout"
 import About from "../components/About"
 import Capacities from "../components/Capacities"
 import FloatingButton from "../components/FloatingButton/index"
+<<<<<<< HEAD
 import Companies from '../components/Companies'
+=======
+import Cases from "../components/Cases"
+>>>>>>> feat(pages): add new components cases
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Badico Cloud Engineering | We are you IT departament" />
     <HeroLayout />
     <FloatingButton />
+<<<<<<< HEAD
     <Companies/>
+=======
+>>>>>>> feat(pages): add new components cases
     <CraftmanshipLayout />
     <Capacities />
+    <Cases />
     <About />
   </Layout>
 )
