fc4e8084553b1dac7c9f47f959d64a5539897b07
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-20 17:02:51 -0300

    style(components/case): add styled cases

---
+++ src/components/Cases/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const CaseWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 2rem;
+  font-family: GalanoGrotesque;
+  align-items: center;
+`
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 500;
+`
+
+export const View = styled.button`
+  border: 1px solid black;
+  border-radius: 38px;
+  width: 6rem;
+  font-size: 0.8rem;
+  height: 1.6rem;
+  padding: 0 0 1rem 0;
+  background: transparent;
+`
