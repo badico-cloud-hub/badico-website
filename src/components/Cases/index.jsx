b98b3d07d1579f84014a6612503fae409d5cce51
Wellington Gomes <wellingtond.gomes@hotmail.com>
2020-07-20 17:02:30 -0300

    feat(components): add new components cases

---
+++ src/components/Cases/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Container from "../Container"
+import { CaseWrapper, View, Title } from "./styled"
+
+const Cases = () => {
+  return (
+    <div>
+      <Container>
+        <CaseWrapper>
+          <Title>Portfolio</Title>
+          <View>View more</View>
+        </CaseWrapper>
+      </Container>
+    </div>
+  )
+}
+
+export default Cases
