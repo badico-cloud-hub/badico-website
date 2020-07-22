90748ff1dcb3415be36ccce2f8d92dc33da81dde
ydrouulis <lombardi.droulis@gmail.com>
2020-07-22 12:08:04 -0300

    refac(components/floatingbutton): refac button

--- src/components/FloatingButton/index.jsx
+++ src/components/FloatingButton/index.jsx
@@ -3,18 +3,31 @@ import FloatWrap from "./FloatWrap"
 import Calendly from "../Calendly"
 import Typeform from "../Typeform/index"
 import { Chat } from "../../images/components/Chat/index"
+import Calendar from "../../images/calendar.svg"
+import Email from "../../images/email.svg"
 
 const FloatingButton = () => {
   return (
     <FloatWrap>
+      <div className="back cloud"></div>
+      <div className="div1 cloud"></div>
+      <div className="div2 cloud"></div>
+      <div className="div3 cloud"></div>
+      <div className="div4 cloud"></div>
       <div className="icon">
         <Chat className="chat" />
       </div>
       <div className="buttonsFilho">
-        <Typeform />
-        <button>
+        <Typeform className="contact" />
+        <div className="iconCalendar">
+          <Calendar />
+        </div>
+        <button className="call">
           <Calendly />
         </button>
+        <div className="iconEmail">
+          <Email/>
+        </div>
       </div>
     </FloatWrap>
   )
