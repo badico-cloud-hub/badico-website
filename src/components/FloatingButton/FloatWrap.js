b0018750f59e26b58871c36e3e3f823305201fa9
ydrouulis <lombardi.droulis@gmail.com>
2020-07-22 12:08:04 -0300

    refac(components/floatingbutton): refac button

--- src/components/FloatingButton/FloatWrap.js
+++ src/components/FloatingButton/FloatWrap.js
@@ -1,34 +1,157 @@
 import styled from "styled-components"
 import media from "styled-media-query"
+import Typeform from "../Typeform/index"
 
 const FloatWrap = styled.div`
-display: flex;
-align-items: center;
-position: fixed;
-top: 90%;
-right: 5%;
-width: 300px;
-z-index: 1;
-cursor: pointer;
-
-.icon{
-  background: #27debf;
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  left: 80%;
-  border-radius: 50%;
-  box-shadow: 0 0 7px black;
   display: flex;
-  justify-content: center;
   align-items: center;
-  z-index: 2;
-}
+  position: fixed;
+  top: 87%;
+  right: 0%;
+  width: 300px;
+  z-index: 1;
+  cursor: pointer;
+
+  .back{
+    display: none;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, .3);
+    position: absolute;
+    top: -466px;
+    right: 0;
+  }
+
+  .div1{
+    display: none;
+    background: white;
+    border-radius: 50%;
+    width: 70px;
+    height: 70px;
+    position: fixed;
+    right: -37px;
+    top: 84%;
+  }
+
+  .div2{
+    display: none;
+    background: white;
+    border-radius: 50%;
+    width: 70px;
+    height: 70px;
+    position: fixed;
+    top: 95%;
+    right: 5px;
+  }
+
+  .div3{
+    display: none;
+    background: white;
+    border-radius: 50%;
+    width: 75px;
+    height: 75px;
+    position: fixed;
+    top: 86%;
+    right: 7px;
+  }
+
+  .div4{
+    display: none;
+    background: white;
+    border-radius: 50%;
+    width: 70px;
+    height: 70px;
+    position: fixed;
+    top: 90%;
+    right: -15px;
+  }
+
+  .icon {
+    display: none;
+    background: #27debf;
+    width: 50px;
+    height: 50px;
+    position: fixed;
+    top: 88.5%;
+    right: 20px;
+    border-radius: 50%;
+    box-shadow: 0 0 7px black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+  }
+
+  .buttonsFilho{
+    display: none;
+  }
+
+  .iconCalendar{
+    background: #fff;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    box-shadow: 0 0 7px black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 30px;
+    left: 200px;
+    z-index: 3;
+  }
+
+  .call{
+    background: #fff;
+    border: none;
+    width: 150px;
+    position relative;
+    left: -140px;
+    border-radius: 13px;
+    font-size: .8rem;
+    padding-bottom: 0.3rem; 
+    padding-right: 2rem;
+  }
+
+  .call a{
+    font-family: GalanoGrotesque;
+    color: #000;
+    text-decoration: none;
+  }
+
+  .iconEmail{
+    background: #fff;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    box-shadow: 0 0 7px black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 100px;
+    left: 290px;
+    z-index: 3;
+  }  
+
+  :hover {
+    .cloud, .buttonsFilho{
+      display: block;
+    }
+  }
+
+  .back:hover{
+    display: none;
+  }
+
+  ${media.greaterThan("760px")`
+
 
 .buttonsFilho{
   display: none;
-  position: absolute;
-  left: 5%;
+  position: fixed;
+  right: 30px;
+  top: 90%;
   overflow: hidden;
   width: 260px;
   height: 30px;
@@ -39,6 +162,11 @@ cursor: pointer;
   padding-top: .1rem;
 }
 
+.call{
+  top: 0;
+  left: 0;
+}
+
 .buttonsFilho button{
   font-size: .8rem;
   background: transparent;
@@ -46,9 +174,6 @@ cursor: pointer;
   padding: 0 0 .25rem 0;
 }
 
-.contact:focus {
-  outline: none;
-}
 
 .buttonsFilho button a{
   color: #FFF;
@@ -56,14 +181,24 @@ cursor: pointer;
   height: 100%;
   font-family: GalanoGrotesque;
   text-decoration: none;
-  padding-left: 1.5rem;
+}
+
+.buttonsFilho button:focus{
+  outline: none;
 }
 
 :hover{
   .buttonsFilho{
     display: block;
   }
+  .buttonsFilho .iconCalendar, .buttonsFilho .iconEmail{
+    display: none;
+  }
+  .cloud{
+    display: none;
+  }
 }
+`}
 `
 
 export default FloatWrap
