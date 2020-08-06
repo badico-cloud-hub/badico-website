987926bf71110b285f8cd5f872efdc92d6fa577f
ydrouulis <lombardi.droulis@gmail.com>
2020-08-06 17:46:36 -0300

    feat: config createpage api

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   if (stage === "build-html") {
     actions.setWebpackConfig({
@@ -11,4 +13,46 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
       },
     })
   }
+}
+
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = require.resolve(`./src/components/TemplatePost/index.jsx`)
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: blogPostTemplate,
+      context: {
+        // additional data can be passed via context
+        slug: node.frontmatter.slug,
+      },
+    })
+  })
 }
