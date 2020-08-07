e4d4e5cf39ec9b830dd7b310c91202afea42ae63
ydrouulis <lombardi.droulis@gmail.com>
2020-08-07 14:52:22 -0300

    chore(components/postlink): add new filds

--- src/components/PostLink/index.jsx
+++ src/components/PostLink/index.jsx
@@ -4,7 +4,11 @@ import { Link } from "gatsby"
 const PostLink = ({ post }) => (
   <div>
     <Link to={post.frontmatter.slug}>
-      {post.frontmatter.title} ({post.frontmatter.date})
+      {post.frontmatter.title} 
+      ({post.frontmatter.date}){" "} 
+      {post.frontmatter.type} {" "}
+      {post.frontmatter.tag} {" "}
+      {post.frontmatter.author}
     </Link>
   </div>
 )
