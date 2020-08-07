6c918327101f968342a560f663dfb8312951658f
ydrouulis <lombardi.droulis@gmail.com>
2020-08-07 17:12:52 -0300

    feat(pages): add search filter

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import PostLink from "../components/PostLink/index.jsx"
 
@@ -7,14 +7,20 @@ const IndexPage = ({
     allMarkdownRemark: { edges },
   },
 }) => {
+
+  const [ author, setAuthor] = useState("")
+  
   const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .filter(edge => edge.node.frontmatter.author === author) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+
 
   return (
     <>
       <div>{Posts}</div>
       <a href="/usecase">Use case</a>
+
+      <input id="search" type="text" value={author} onChange={event => setAuthor(event.target.value)}></input>
     </>
   )
 }
