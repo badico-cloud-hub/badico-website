46b106424c006bed84a128a29a91e53ac687b889
ydrouulis <lombardi.droulis@gmail.com>
2021-03-26 14:17:48 -0300

    chore(components/capacities): use width props

--- src/components/Capacities/index.jsx
+++ src/components/Capacities/index.jsx
@@ -16,35 +16,35 @@ const Capacities = () => {
       <Container>
         <Title>Capacities</Title>
         <Cards>
-          <Images>
+          <Images width='92%'>
             <Mobile className="img mobile" />
             <p className="mobileText">Mobile development with flutter</p>
           </Images>
-          <Images>
+          <Images width='92%'>
             <Web className="img" />
             <p>Web development with vue and react</p>
           </Images>
-          <Images>
+          <Images width='92%'>
             <Clean className="img" />
             <p>Clean software architeture</p>
           </Images>
-          <Images>
-            <Infra className="img" />
+          <Images width='92%'>
+            <Infra className="img"/>
             <p>Infra as code</p>
           </Images>
-          <Images>
+          <Images width='60%'>
             <Cloud className="img" />
             <p>Cloud architeture</p>
           </Images>
-          <Images>
+          <Images width='60%'>
             <Serverless className="img" />
             <p>Serverless architeture</p>
           </Images>
-          <Images>
+          <Images width='92%'>
             <Continuos className="img" />
             <p>Continuous integration and delivery</p>
           </Images>
-          <Images>
+          <Images width='92%'>
             <Ux className="img" />
             <p>UX strategy</p>
           </Images>
