05f9e3832d7ebcaf6ec6e8bab4d687f239db5115
ydrouulis <lombardi.droulis@gmail.com>
2021-03-26 14:36:06 -0300

    chore(pages) change url layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import CraftmanshipLayout from "../components/CraftmanshipLayout/index"
-import Layout from "../components/layout"
+import Layout from "../components/Layout/layout"
 import SEO from "../components/seo"
 import HeroLayout from "../components/HeroLayout"
 import About from "../components/About"
