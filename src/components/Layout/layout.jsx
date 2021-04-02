7c405759d6459040add2b2645b985700e453d1d9
Antonio <antonioluciofb@hotmail.com>
2021-04-02 16:48:15 -0300

    refac(components/layout): add bg and styles

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -2,14 +2,19 @@ import React from "react"
 import PropTypes from "prop-types"
 import Header from "../Header"
 import Footer from "../Footer"
+import Container from "../Container"
+import Bg from "../../images/bgDesktop.svg"
 
-import { LayoutWrapper } from './style'
+import { LayoutWrapper } from "./style"
 
 const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
       <Header />
-      <main>{children}</main>
+      <Bg fill="#0e273b" className="bgWave" />
+      <Container>
+        <main>{children}</main>
+      </Container>
       <Footer />
     </LayoutWrapper>
   )
