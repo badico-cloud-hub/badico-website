026522e205a86938ee4197428eff634414be3f1a
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-06 14:47:02 -0300

    feat(components/about): Add Multilanguage

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,20 +1,32 @@
 import React from "react"
 import Container from "../Container/index"
 import * as S from "./styled"
+import { useTranslation } from "react-i18next"
 
 const About = () => {
+  const { t, i18n } = useTranslation()
+
   return (
     <S.AboutUs>
       <Container>
-        <h1>About us</h1>
+        <h1
+          onMouseLeave={() => {
+            i18n.changeLanguage("en")
+          }}
+          onMouseEnter={() => {
+            i18n.changeLanguage("pt")
+          }}
+        >
+          {t("AboutUs")}
+        </h1>
         <h2>Who we are</h2>
-        <S.Text class="text-about" width='92.5%'>
+        <S.Text class="text-about" width="92.5%">
           We specialize in web and mobile development, providing all stages of
           the product, from the first design to the final product for users. Our
           methodology allows us to integrate companies in a homogeneous way,
           building and developing the company's technology department together.
         </S.Text>
-        <S.Text class="text-about" width='90%'>
+        <S.Text class="text-about" width="90%">
           If your company doesn't just want to develop an application, but build
           a product that proudly generates knowledge and impact, we can and want
           to help you.
