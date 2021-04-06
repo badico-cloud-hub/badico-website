2dcf3490f62262afc57ef784d4a874455dfb5ba3
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-06 14:59:54 -0300

    feat(pages): add i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,23 @@ import HeroLayout from "../components/HeroLayout"
 import About from "../components/About"
 import Capacities from "../components/Capacities"
 import FloatingButton from "../components/FloatingButton/index"
-import Companies from '../components/Companies'
+import Companies from "../components/Companies"
+
+import { I18nextProvider } from "react-i18next"
+import i18n from "../i18n"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Badico Cloud Engineering | We are you IT departament" />
-    <HeroLayout />
-    <FloatingButton />
-    <Companies/>
-    <CraftmanshipLayout />
-    <Capacities />
-    <About />
-  </Layout>
+  <I18nextProvider i18n={i18n}>
+    <Layout>
+      <SEO title="Badico Cloud Engineering | We are you IT departament" />
+      <HeroLayout />
+      <FloatingButton />
+      <Companies />
+      <CraftmanshipLayout />
+      <Capacities />
+      <About />
+    </Layout>
+  </I18nextProvider>
 )
 
 export default IndexPage
