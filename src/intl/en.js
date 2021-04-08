8355a246849e4456e467ce1ab14722c47b2fc293
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-08 18:24:46 -0300

    feat(intl): add configs en words for tranlations

---
+++ src/intl/en.js
@@ -0,0 +1,41 @@
+export default {
+  "menu.div.options.0": "Home",
+  "menu.div.options.1": "Capacities",
+  "menu.div.options.2": "Our Process",
+  "menu.div.options.3": "Our Team",
+  "menu.div.options.4": "About Us",
+  "menu.div.options.5": "Contact Us",
+  "hero.info.h1": "IT department Headache-free for startups",
+  "hero.info.p": "We are your IT department",
+  "hero.buttons.0": "Contact us",
+  "hero.buttons.1": "Schedule a call",
+  "companies.text": "Companies that trust us",
+  "craftmanship.text":
+    "We deliver value every week to our clients. Value means code in production ready.",
+  "cards.card.0": "Not only working software, but also ",
+  "cards.card.0.bold": "well-crafted software",
+  "cards.card.1": "Not only responding to change, but also ",
+  "cards.card.1.bold": "steadily adding value",
+  "cards.card.2": "Not only individuals and interactions, but also ",
+  "cards.card.2.bold": "a community of professionals",
+  "cards.card.3": "Not only customer collaboration, but also ",
+  "cards.card.3.bold": "productive partnerships.",
+  "capacities.title": "Capacities",
+  "capacities.cards.0": "Mobile development with flutter",
+  "capacities.cards.1": "Web development with vue and react",
+  "capacities.cards.2": "Cloud architeture",
+  "capacities.cards.3": "Serverless architeture",
+  "capacities.cards.4": "Clean software architeture",
+  "capacities.cards.5": "Infra as code",
+  "capacities.cards.6": "Continuous integration and continuous delivery",
+  "capacities.cards.7": "UX strategy",
+  "about.h1": "About",
+  "about.h2": "Who we are",
+  "about.text.0": `We specialize in web and mobile development, providing all stages of
+  the product, from the first design to the final product for users. Our
+  methodology allows us to integrate companies in a homogeneous way,
+  building and developing the company's technology department together`,
+  "about.text.1": `If your company doesn't just want to develop an application, but build
+  a product that proudly generates knowledge and impact, we can and want
+  to help you.`,
+}
