e8b50cc0af16d859f577fcd8dee7e0f464058c66
Fabio Seixas <flscosta94@gmail.com>
2021-04-08 12:27:03 -0300

    feat:(): just send to remote repo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,19 @@
-import React, { useEffect } from "react"
+import React from "react"
+import LanguageProvider from "../providers/LanguageProvider"
+import MainPage from '../components/Main'
+import { WebsiteProvider } from '../../contexts'
 
-import CraftmanshipLayout from "../components/CraftmanshipLayout/index"
-import Layout from "../components/Layout/layout"
-import SEO from "../components/seo"
-import HeroLayout from "../components/HeroLayout"
-import About from "../components/About"
-import Capacities from "../components/Capacities"
-import FloatingButton from "../components/FloatingButton/index"
-import Companies from "../components/Companies"
-import Cases from "../components/Cases"
-import TechStacks from "../components/TechStacks"
-import LanguageProvider from "../components/LanguageProvider"
 
-const IndexPage = () => (
-  <LanguageProvider language="en">
-    <Layout>
-      <SEO title="Badico Cloud Engineering | We are you IT departament" />
-      <HeroLayout />
-      <FloatingButton />
-      <Companies />
-      <CraftmanshipLayout />
-      <TechStacks />
-      <Capacities />
-      <Cases />
-      <About />
-    </Layout>
-  </LanguageProvider>
-)
+const language = "en"
+
+const IndexPage = () => {
+  
+  return (
+    <WebsiteContext>
+      <LanguageProvider >
+        <MainPage language={language}/>
+      </LanguageProvider>
+    </WebsiteContext>
+  )}
 
 export default IndexPage
