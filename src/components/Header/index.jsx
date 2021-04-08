acc4216f8d809b0ab1fcfd43186b23d5e3fcd6c4
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-08 18:21:52 -0300

    feat(components/header): add i18n settings

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -57,36 +57,34 @@ const Header = () => {
               <a href="#">Meet Our Time</a>
             </li> */}
             <li>
-              <a href="#capacities">
-                {lang[language].capacities}
-              </a>
+              <a href="#capacities">{lang[language]["menu.div.options.1"]}</a>
             </li>
             {/* <li>
               <a href="#">Blog</a>
             </li> */}
-              <li>
-                <a href="#about">About us</a>
-              </li>
-              <li>
-                <a
-                  onClick={() => {
-                    form.open()
-                  }}
-                >
-                  Contact us
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <button
-            className="close"
-            onClick={() => {
-              setIsOpen(false)
-            }}
-          >
-            <Close />
-          </button>
-        </div>
+            <li>
+              <a href="#about">{lang[language]["menu.div.options.4"]}</a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  form.open()
+                }}
+              >
+                {lang[language]["menu.div.options.5"]}
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button
+          className="close"
+          onClick={() => {
+            setIsOpen(false)
+          }}
+        >
+          <Close />
+        </button>
+      </div>
       </Styled.Container>
     </Styled.HeaderWrapper>
   )
