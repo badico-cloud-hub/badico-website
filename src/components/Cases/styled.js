343320a5245a0c8978c957a23f0e2ecb6b41ef81
EdJunior0 <edsonjunior.1219@gmail.com>
2021-04-08 13:39:28 -0300

    feat(components/cases): change style of index

--- src/components/Cases/styled.js
+++ src/components/Cases/styled.js
@@ -2,32 +2,26 @@ import styled from "styled-components"
 
 export const Case = styled.div`
   display: flex;
-  margin: 0 2rem 3.5rem 2rem;
 `
 
 export const CaseWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  font-family: GalanoGrotesque;
+  justify-content: center;
+  font-family: Roboto;
   align-items: center;
   margin-bottom: 1.5rem;
+
+  @media(min-width: 768px){
+    margin-bottom: 3.5rem;
+  }
 `
 
 export const Title = styled.h1`
-  font-size: 1.5rem;
+  font-size: 1.25rem;
   font-weight: 500;
-`
+  margin: 0;
 
-export const View = styled.button`
-  border: 1px solid black;
-  border-radius: 38px;
-  width: 6rem;
-  font-size: 0.8rem;
-  height: 1.6rem;
-  padding: 0 0 1rem 0;
-  background: transparent;
-
-  :focus {
-    outline: none;
+  @media(min-width: 768px){
+    font-size: 1.5rem;
   }
 `
