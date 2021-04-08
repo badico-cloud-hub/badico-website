a714ff2d1a7f912e0b689309b6fc745b9fae09a6
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-08 18:16:27 -0300

    feat(components/capacities): add i18n settings

--- src/components/Capacities/index.jsx
+++ src/components/Capacities/index.jsx
@@ -9,44 +9,45 @@ import Serverless from "../../images/capacities/Serverless-arquiteture.svg"
 import Continuos from "../../images/capacities/Continuos.svg"
 import Ux from "../../images/capacities/UX-strategy.svg"
 import { Title, CapacitiesWrapper, Images, Cards } from "./styled"
+import lang from "../../intl/langs"
 
-const Capacities = () => {
+const Capacities = ({ language }) => {
   return (
     <CapacitiesWrapper>
       <Container>
-        <Title id="capacities">Capacities</Title>
+        <Title id="capacities">{lang[language]["capacities.title"]}</Title>
         <Cards>
-          <Images pMarginTop='10px' width='92%'>
+          <Images pMarginTop="10px" width="92%">
             <Mobile className="img mobile" />
-            <p className="mobileText">Mobile development with flutter</p>
+            <p className="mobileText">{lang[language]["capacities.cards.0"]}</p>
           </Images>
-          <Images pMarginTop='10px' width='92%'>
+          <Images pMarginTop="10px" width="92%">
             <Web className="img" />
-            <p>Web development with vue and react</p>
+            <p>{lang[language]["capacities.cards.1"]}</p>
           </Images>
-          <Images pMarginTop='18px' width='60%'>
+          <Images pMarginTop="18px" width="60%">
             <Cloud className="img" />
-            <p>Cloud architeture</p>
+            <p>{lang[language]["capacities.cards.2"]}</p>
           </Images>
-          <Images pMarginTop='18px' width='60%'>
+          <Images pMarginTop="18px" width="60%">
             <Serverless className="img" />
-            <p>Serverless architeture</p>
+            <p>{lang[language]["capacities.cards.3"]}</p>
           </Images>
-          <Images pMarginTop='18px' width='92%'>
+          <Images pMarginTop="18px" width="92%">
             <Clean className="img" />
-            <p>Clean software architeture</p>
+            <p>{lang[language]["capacities.cards.4"]}</p>
           </Images>
-          <Images pMarginTop='18px' width='92%'>
-            <Infra className="img"/>
-            <p>Infra as code</p>
+          <Images pMarginTop="18px" width="92%">
+            <Infra className="img" />
+            <p>{lang[language]["capacities.cards.5"]}e</p>
           </Images>
-          <Images pMarginTop='10px' width='92%'>
+          <Images pMarginTop="10px" width="92%">
             <Continuos className="img" />
-            <p>Continuous integration and continuous delivery</p>
+            <p>{lang[language]["capacities.cards.6"]}</p>
           </Images>
-          <Images pMarginTop='10px' width='92%' >
+          <Images pMarginTop="10px" width="92%">
             <Ux className="img" />
-            <p>UX strategy</p>
+            <p>{lang[language]["capacities.cards.7"]}</p>
           </Images>
         </Cards>
       </Container>
