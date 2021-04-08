0f242802b54e50ed2d480df66136d3e986cdab14
EdJunior0 <edsonjunior.1219@gmail.com>
2021-04-08 13:40:22 -0300

    feat(components/gallery): change carousel itens

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,74 +1,102 @@
 import React from "react"
 import Carousel from "react-multi-carousel"
 import "react-multi-carousel/lib/styles.css"
-import slide01 from "../../images/cases/slide01.png"
-import slide02 from "../../images/cases/slide02.png"
-import slide03 from "../../images/cases/slide04.png"
-import slide04 from "../../images/cases/slide04.png"
-import { Image } from "./styled"
+import Tailor from "../../images/cases/tailor_logo.svg"
+import Harv from "../../images/cases/harv_logo.svg"
+import Plando from "../../images/cases/plando_logo.svg"
+import Jump from "../../images/cases/jump_logo.png"
+import SlideTailor from "../../images/cases/tailor_case.png"
+import SlideHarv from "../../images/cases/harv_case.png"
+import SlidePlando from "../../images/cases/plando_case.png"
+import SlideJump from "../../images/cases/jump_case.png"
+import { Container } from "./styled"
+import {CardsCase, CardCaseJump} from './CardsCase/CardsCase'
+import { useWindowSize } from "../../hooks/useWindowsSize"
+import { TailorText } from './CardsCase/Texts/TextCases'
 
 const Gallery = () => {
+  const size = useWindowSize();
   return (
-    <div>
-      <div
-        style={{
-          paddingBottom: "30px",
-          position: "relative",
-        }}
-      >
-        <Carousel
-          additionalTransfrom={0}
-          arrows
-          autoPlay
-          autoPlaySpeed={2000}
-          centerMode={true}
-          removeArrowOnDeviceType={["desktop", "mobile", "tablet"]}
-          className=""
-          containerClass="container"
-          dotListClass=""
-          draggable
-          focusOnSelect={false}
-          infinite
-          itemClass=""
-          keyBoardControl
-          minimumTouchDrag={80}
-          renderButtonGroupOutside={false}
-          renderDotsOutside
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 1024,
-              },
-              items: 3,
-            },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0,
-              },
-              items: 2,
-            },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 464,
-              },
-              items: 3,
-            },
-          }}
-          showDots
-          sliderClass=""
-          slidesToSlide={1}
-          swipeable
-        >
-          <Image src={slide01} />
-          <Image src={slide02} />
-          <Image src={slide03} />
-          <Image src={slide04} />
-        </Carousel>
-      </div>
-    </div>
+    <>
+      {size.width < 1024 ? (
+        <Container>
+          <div
+            style={{
+              paddingBottom: "30px",
+              position: "relative",
+            }}
+          >
+            <Carousel
+              additionalTransfrom={0}
+              arrows
+              /*autoPlay
+              autoPlaySpeed={2000}*/
+              centerMode={true}
+              removeArrowOnDeviceType={["desktop", "mobile", "tablet"]}
+              className=""
+              containerClass="container"
+              dotListClass=""
+              draggable
+              focusOnSelect={false}
+              //infinite
+              itemClass="item"
+              keyBoardControl
+              minimumTouchDrag={80}
+              renderButtonGroupOutside={false}
+              renderDotsOutside
+              responsive={{
+                mobile: {
+                  breakpoint: {
+                    max: 768,
+                    min: 0,
+                  },
+                  items: 1,
+                },
+                tablet: {
+                  breakpoint: {
+                    max: 1024,
+                    min: 768,
+                  },
+                  items: 2,
+                },
+              }}
+              showDots
+              slidesToSlide={1}
+              swipeable
+            >
+              <CardsCase
+                Logo={Tailor}
+                Image={SlideTailor}
+                top="-18px"
+                height="12px"
+                children={<TailorText/>}
+              />
+              <CardsCase
+                Logo={Harv}
+                Image={SlideHarv}
+                top="-17px"
+                height="13px"
+              />
+              <CardsCase
+                Logo={Plando}
+                Image={SlidePlando}
+                top="-17px"
+                height="15px"
+              />
+              <CardCaseJump
+                LogoJump={Jump}
+                Image={SlideJump}
+                top="-17px"
+                width="80px"
+                height="15px"
+              />
+            </Carousel>
+          </div>
+        </Container>
+      ) : (
+        null
+      )}
+    </>
   )
 }
 
