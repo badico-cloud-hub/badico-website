97d626fa6499bd8fee3f882dcef6a9aa7a2e2871
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-08 18:16:49 -0300

    feat(components/companies): add i18n settings

--- src/components/Companies/index.jsx
+++ src/components/Companies/index.jsx
@@ -1,26 +1,33 @@
 import React from "react"
-import { Container, TextCompanies, DivImages, PrimaryDiv, SecondDiv } from './styled'
-import Jump from '../../images/jump.png'
-import ZemoPay from '../../images/zemo.svg'
-import Tailor from '../../images/tailor.svg'
-import Harv from '../../images/harv.svg'
-import Bizaline from '../../images/bizaline.svg'
+import {
+  Container,
+  TextCompanies,
+  DivImages,
+  PrimaryDiv,
+  SecondDiv,
+} from "./styled"
+import Jump from "../../images/jump.png"
+import ZemoPay from "../../images/zemo.svg"
+import Tailor from "../../images/tailor.svg"
+import Harv from "../../images/harv.svg"
+import Bizaline from "../../images/bizaline.svg"
+import lang from "../../intl/langs"
 
-const Companies = () => {
-  return(
+const Companies = ({ language }) => {
+  return (
     <Container>
-      <TextCompanies>Companies that trust us</TextCompanies>
-        <DivImages>
-          <PrimaryDiv>
-            <img src={Jump} className='logoJump'/>
-            <ZemoPay className='logoZemo'/>
-            <Tailor className='logoTailor'/>
-          </PrimaryDiv>
-          <SecondDiv>
-            <Harv className='logoHarv'/>
-            <Bizaline className='logoBizaline'/>
-          </SecondDiv>
-        </DivImages>
+      <TextCompanies>{lang[language]["companies.text"]}</TextCompanies>
+      <DivImages>
+        <PrimaryDiv>
+          <img src={Jump} className="logoJump" />
+          <ZemoPay className="logoZemo" />
+          <Tailor className="logoTailor" />
+        </PrimaryDiv>
+        <SecondDiv>
+          <Harv className="logoHarv" />
+          <Bizaline className="logoBizaline" />
+        </SecondDiv>
+      </DivImages>
     </Container>
   )
 }
