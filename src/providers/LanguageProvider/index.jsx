4fbc06e71197547e1c86e14ad194694be95ca3f2
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-08 18:30:07 -0300

    refac(providers/languageprovider): fix logic problem

--- src/providers/LanguageProvider/index.jsx
+++ src/providers/LanguageProvider/index.jsx
@@ -1,39 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
 
-import { getUserLangKey } from 'ptz-i18n';
-import { withPrefix } from 'gatsby-link';
-import { WebsiteProvider, useWebsiteHook } from '../../contexts'
+import { getUserLangKey } from "ptz-i18n"
+import { withPrefix } from "gatsby-link"
+import { WebsiteProvider, useWebsiteHook } from "../../contexts"
 
 const languages = {
-  langs: ['en', 'pt'],
-  defaultLangKey: '/'
-};
+  langs: ["en", "pt"],
+  defaultLangKey: "/",
+}
 
 const LanguageProvider = ({ children }) => {
-  const website = useWebsiteHook();
-  console.log('WEB: ', website)
-  const { language, setLanguage } = website;
+  const website = useWebsiteHook()
+  const { language, setLanguage } = website
 
   useEffect(() => {
     if (!language) {
-      const { langs, defaultLangKey } = languages;
-      const langKey = getUserLangKey(langs, defaultLangKey);
-      
+      const { langs, defaultLangKey } = languages
+      const langKey = getUserLangKey(langs, defaultLangKey)
+
       setLanguage(langKey)
-      
-      if (langKey === "en") {
-        //...
-      } else {
-        const homeUrl = withPrefix(`/${langKey}/`);
+
+      if (
+        langKey !== "en" &&
+        window.location.pathname.indexOf(`/${langKey}`) === -1
+      ) {
+        const homeUrl = withPrefix(`/${langKey}`)
         window.location.href = homeUrl + window.location.pathname
       }
     }
   }, [])
 
-  return (
-    <WebsiteProvider>
-      {children}
-    </WebsiteProvider>
-  )}
+  return <WebsiteProvider>{children}</WebsiteProvider>
+}
 
 export default LanguageProvider
