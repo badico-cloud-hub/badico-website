3857eb7eb8c782d3ad8f48f4f5a49864e31f5446
Fabio Seixas <flscosta94@gmail.com>
2021-04-08 08:08:23 -0300

    feat(intl/lang): create lang map

---
+++ src/intl/langs.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pt: {
+    "home": "Pagina Inicial",
+    "capacities": "Habilidades",
+    "about": "Sobre Nós",
+    "contact": "Contato"
+  },
+  en: {
+    "home": "Home",
+    "capacities": "Capacities",
+    "about": "About Us",
+    "contact": "Contact"
+  }
+}
