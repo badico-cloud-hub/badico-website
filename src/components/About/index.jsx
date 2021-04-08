31b3bacd7dc4b578cf456e2becc54aeccfbd08fb
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-08 17:45:09 -0300

    feat(components/about): add i18n settings

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,26 +1,19 @@
 import React from "react"
 import Container from "../Container/index"
 import * as S from "./styled"
-import { useTranslation } from "react-i18next"
+import lang from "../../intl/langs"
 
-const About = () => {
-  const { t, i18n } = useTranslation()
-
+const About = ({ language }) => {
   return (
     <S.AboutUs>
       <Container>
-        <h1>About</h1>
-        <h2>Who we are</h2>
+        <h1>{lang[language]["about.h1"]}</h1>
+        <h2>{lang[language]["about.h2"]}</h2>
         <S.Text class="text-about" width="92.5%">
-          We specialize in web and mobile development, providing all stages of
-          the product, from the first design to the final product for users. Our
-          methodology allows us to integrate companies in a homogeneous way,
-          building and developing the company's technology department together.
+          {lang[language]["about.text.0"]}
         </S.Text>
         <S.Text class="text-about" width="90%">
-          If your company doesn't just want to develop an application, but build
-          a product that proudly generates knowledge and impact, we can and want
-          to help you.
+          {lang[language]["about.text.1"]}
         </S.Text>
       </Container>
     </S.AboutUs>
