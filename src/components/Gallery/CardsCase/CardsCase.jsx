a268d2ab3be2a81eeb77bd8b0af656196c2c4b5e
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-09 18:21:43 -0300

    feat(components/gallery/cardcase): add animations

--- src/components/Gallery/CardsCase/CardsCase.jsx
+++ src/components/Gallery/CardsCase/CardsCase.jsx
@@ -1,62 +1,68 @@
-import React, {useState} from 'react'
-import { Render, Content, Image, DivText, Title, Text } from './styled'
+import React, { useState } from "react"
+import { Render, Content, Image, DivText, Title, Text } from "./styled"
 
-export const CardsCase = (props) => {
-  const [state, setState] = useState(false);
+export const CardsCase = props => {
+  return (
+    <>
+      <Render className="front">
+        <Content>
+          <img src={props.Image} />
+          <Image style={{ top: props.top }}>
+            <props.Logo style={{ height: props.height, width: props.width }} />
+          </Image>
+        </Content>
+      </Render>
+      <Render className="back">
+        <Content>
+          <DivText>{props.children}</DivText>
+          <Image style={{ top: props.top }}>
+            <props.Logo style={{ height: props.height, width: props.width }} />
+          </Image>
+        </Content>
+      </Render>
+    </>
+  )
+}
+
+export const CardCaseJump = props => {
+  const [state, setState] = useState(false)
   console.log(state)
-  return(
+  return (
     <>
       {state === false ? (
         <Render onClick={() => setState(!state)}>
           <Content>
-            <img src={props.Image}/>
-            <Image style={{top: props.top}}>
-              <props.Logo style={{height: props.height, width: props.width}}/>
+            <img src={props.Image} />
+            <Image
+              style={{
+                top: props.top,
+                height: props.height,
+                width: props.width,
+              }}
+            >
+              <img src={props.LogoJump} />
             </Image>
           </Content>
         </Render>
       ) : (
         <Render onClick={() => setState(!state)}>
           <Content>
-          <DivText>
-            {props.children}
-          </DivText>
-          <Image style={{top: props.top}}>
-            <props.Logo style={{height: props.height, width: props.width}}/>
-          </Image>
+            <DivText>{props.children}</DivText>
+            <Image
+              style={{
+                top: props.top,
+                height: props.height,
+                width: props.width,
+              }}
+            >
+              <img
+                src={props.LogoJump}
+                style={{ height: props.height, width: props.width }}
+              />
+            </Image>
           </Content>
         </Render>
       )}
     </>
-  )
-}
-
-export const CardCaseJump = (props) => {
-  const [state, setState] = useState(false);
-  console.log(state)
-  return(
-    <>
-      {state === false ? (
-          <Render onClick={() => setState(!state)}>
-            <Content>
-              <img src={props.Image}/>
-              <Image style={{top: props.top, height: props.height, width: props.width}}>
-                <img src={props.LogoJump}/>
-              </Image>
-            </Content>
-          </Render>
-        ) : (
-          <Render onClick={() => setState(!state)}>
-            <Content>
-              <DivText>
-                {props.children}
-              </DivText>
-              <Image style={{top: props.top, height: props.height, width: props.width}}>
-                <img src={props.LogoJump} style={{height: props.height, width: props.width}}/>
-              </Image>
-            </Content>
-          </Render>
-        )}
-      </>
   )
 }
