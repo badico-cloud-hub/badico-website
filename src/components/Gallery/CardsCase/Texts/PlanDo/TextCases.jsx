4580a3be182b583972f255ac060b53e75a364bdc
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-09 18:19:26 -0300

    feat(components/gallery/cardscase/texts/plando): add components for text card

---
+++ src/components/Gallery/CardsCase/Texts/PlanDo/TextCases.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Title } from './styled.js'
+
+export const PlanDoText = () => {
+  return(
+    <>
+      <Title><strong>Languages:</strong> javascrip, golang, python</Title>
+      <Title><strong>Services amount:</strong> 3</Title>
+      <Title><strong>Professinals:</strong> 2</Title>
+      <Title><strong>Development time:</strong> 8 meses</Title>
+      <Title><strong>Tools:</strong> AWS Lambda, express, ...</Title>
+    </>    
+  )
+}
