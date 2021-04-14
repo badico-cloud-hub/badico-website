a4ae1464fc4019a22909c48f96cf8d0c13609043
EdJunior0 <edsonjunior.1219@gmail.com>
2021-04-14 17:31:56 -0300

    feat(components/gallery): change style of items

--- src/components/Gallery/styled.js
+++ src/components/Gallery/styled.js
@@ -1,20 +1,40 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  .container {
-    max-width: 1300px;
 
-    @media(min-width: 768px){
-      min-width: 768px;
+  margin: 0 0 0 17px;
+
+  @media(min-width: 1366px){
+    margin: 0;
+  }
+
+  .checkBoxInput{
+    display: none;
+  }
+
+  .cardContainer{
+    height: 100%;
+  }
+
+  label{
+    margin: 0;
+    height: 100%;
+  }
+
+  .container{
+    max-width: 1366px;
+
+    @media(min-width: 1366px){
+      justify-content: center;
     }
   }
 
   .div{
-    padding-bottom: 30px;
+    padding-bottom: 14px;
     position: relative;
 
     @media(min-width: 768px){
-      padding: 0px;
+      padding-bottom: 0;
     }
   }
 
@@ -24,83 +44,82 @@ export const Container = styled.div`
   }
 
   .react-multi-carousel-track {
-    align-items: center;
-    height: 167px;
-
-    @media(min-width: 360px){
-      li:last-child{
-        margin: 0px;
-      }
-    }
+    height: 203px;
 
     @media(min-width: 768px){
-      height: 333px;
+      height: 313px;
     }
 
-    @media(min-width: 1024px){
-      li:last-child{
-        margin-left: 17px;
-      }
-      li:first-child{
-        margin-left: 0px;
-      }
+    @media(min-width: 1366px){
+      height: 313px;
+    }
+
+    li:last-child{
+      margin: 0;
     }
   }
 
   .react-multi-carousel-list{
-    height: 200px;
+    height: 235px;
 
     @media(min-width: 768px){
-      height: 425px;
+      height: 393px;
+    }
+
+    @media(min-width: 1366px){
+      height: 387px;
     }
   }
 
+  ul .react-multi-carousel-item--active{
+    opacity: 1;    
+  }
+
   .item {
-    max-width: 138px;
-    min-width: 138px;
-    max-height: 180px;
+    max-width: 178px;
+    height: 100%;
     margin: 0 17px 0 0;
     transition: all 1s linear;
     perspective: 600px;
+    opacity: 0.6;
 
     @media(min-width: 768px){
+      min-width: 261px;
       max-width: 281px;
-      min-width: 281px;
-      max-height: 333px;
     }
 
-    @media(min-width: 1024px){
-      max-width: 228px;
-      min-width: 228px;
-      max-height: 274px;
-      margin: 0 0 0 17px;
+    @media(min-width: 1366px){
+      min-width: 248px;
+      max-width: 248px;
+      margin: 0 31px 0 0;
     }
   }
 
   .item:hover > .front {
-    transition: all 0.6s linear;
+    transition: all 0.4s linear;
     transform: rotateY(-180deg);
   }
+
   .item:hover > .back {
-    transition: all 0.6s linear;
+    transition: all 0.4s linear;
     transform: rotateY(0deg);
   }
 
   .front {
-    transition: all 0.6s linear;
+    transition: all 0.4s linear;
     top: 0;
     z-index: 1;
   }
 
   .back {
-    transition: all 0.6s linear;
+    transition: all 0.4s linear;
     position: absolute;
     top: 0;
     z-index: -1;
     transform: rotateY(180deg);
   }
 
-  @media (min-width: 1024px) {
+  @media (min-width: 1366px) {
     width: 100%;
     display: flex;
     flex-direction: row;
