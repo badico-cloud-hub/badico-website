0748a0825d9d4b27c08f4784149fdc0ef15631ad
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-16 16:22:22 -0300

    feat(pages/on-demand): add language

--- src/pages/on-demand.pt.js
+++ src/pages/on-demand.pt.js
@@ -1,21 +1,18 @@
 import React from "react"
-
 import LanguageProvider from "../providers/LanguageProvider"
-import { WebsiteProvider } from '../contexts'
-import Layout from '../components/LayoutLandingPage'
-
+import { WebsiteProvider } from "../contexts"
+import Layout from "../components/LayoutLandingPage"
 
 const language = "pt"
 
 const IndexPage = () => {
-  
   return (
     <WebsiteProvider>
-      <LanguageProvider >
-        <Layout language={language}>
-        </Layout>
+      <LanguageProvider>
+        <Layout language={language} />
       </LanguageProvider>
     </WebsiteProvider>
-  )}
+  )
+}
 
 export default IndexPage
