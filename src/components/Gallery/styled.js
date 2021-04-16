9fc0dbad38df3fa88812b56a465fe731b71f731e
EdJunior0 <edsonjunior.1219@gmail.com>
2021-04-16 17:24:33 -0300

    feat(components/gallery): add webkit to safari

--- src/components/Gallery/styled.js
+++ src/components/Gallery/styled.js
@@ -97,24 +97,27 @@ export const Container = styled.div`
 
   .item:hover > .front {
     transition: z-index 1.1s, opacity 2s, transform 0.4s linear;
+    -webkit-transition: z-index 1.1s, opacity 2s, transform 0.4s linear;
     transform: rotateY(-180deg);
   }
 
   .item:hover > .back {
     transition: z-index 1.1s, transform 0.4s linear;
+    -webkit-transition: z-index 1.1s, transform 0.4s linear;
     z-index: 1;
     transform: rotateY(0deg);
   }
 
   .front {
     transition: z-index 0.5s, transform 0.4s linear;
-    transition: all 0.4s linear;
+    -webkit-transition: z-index 0.5s, transform 0.4s linear;
     top: 0;
     z-index: 1;
   }
 
   .back {
     transition: z-index 0.5s, opacity 1s, transform 0.4s linear;
+    -webkit-transition: z-index 0.5s, opacity 1s, transform 0.4s linear;
     position: absolute;
     top: 0;
     transform: rotateY(180deg);
