5cf49218a5b59adf2df97ac73f2616b2db5a47a7
Lucas Badico <lucasgomesdesantana@gmail.com>
2021-04-18 13:44:13 -0300

    feat(components/cards): edit plando text

--- src/components/Gallery/CardsCase/Texts/PlanDo/TextCases.jsx
+++ src/components/Gallery/CardsCase/Texts/PlanDo/TextCases.jsx
@@ -7,7 +7,7 @@ export const PlanDoText = () => {
       <Title><strong>Javascript, Golang, Python</strong> </Title>
       <Title><strong>Services count:</strong> 4</Title>
       <Title><strong>Team Size:</strong> 3</Title>
-      <Title><strong>Tools:</strong> AWS Lambda, ExpressJs, ReactJs, Websocket, Digital Ocean Droplets, CI/CD, Serverless</Title>
+      <Title><strong>Tools:</strong>ReactJs, Serverless, Websocket, AWS</Title>
     </>    
   )
 }
