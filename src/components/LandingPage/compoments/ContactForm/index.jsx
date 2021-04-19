088cc032f45c0ac754b79265e713728a773b007a
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-19 17:11:27 -0300

    feat(components/landingpage/components): add contact form

---
+++ src/components/LandingPage/compoments/ContactForm/index.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Coder from "../../images/coder.svg"
+import * as Styled from "./style"
+
+const ContactForm = () => {
+  return (
+    <Styled.Container>
+      <Styled.Box>
+        <Styled.DivCoder>
+          <Coder />
+        </Styled.DivCoder>
+        <Styled.Title>
+          Lorem ipsum nisi facilisis torquent suspendisse non
+        </Styled.Title>
+        <Styled.Form method="post">
+          <Styled.Input placeholder="Seu nome" />
+          <Styled.Input placeholder="Seu e-mail" />
+          <Styled.Input placeholder="Lorem ipsum" />
+          <Styled.Button type="submit">
+            Quero ter um time incrível
+          </Styled.Button>
+        </Styled.Form>
+      </Styled.Box>
+    </Styled.Container>
+  )
+}
+
+export default ContactForm
