e609113aec8404ec0e690920bdc5e9e0aa02b34f
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-19 17:11:51 -0300

    feat(components/layoutlandingpage): add contact form

--- src/components/LayoutLandingPage/index.jsx
+++ src/components/LayoutLandingPage/index.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react"
 import Header from "../LandingPage/compoments/Header"
-
+import ContactForm from '../LandingPage/compoments/ContactForm'
 import { LayoutWrapper } from "./style"
 
 const Layout = ({ language, children }) => {
   return (
     <LayoutWrapper>
       <Header language={language}/>
+      
+      <ContactForm language={language} />
     </LayoutWrapper>
   )
 }
