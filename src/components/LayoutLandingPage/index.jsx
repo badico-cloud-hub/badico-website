4a6f0cd47e2501d6979abac113b83c8e60d29585
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-20 13:39:58 -0300

    feat(components/layoutlandingpage): add textandcontact component and container

--- src/components/LayoutLandingPage/index.jsx
+++ src/components/LayoutLandingPage/index.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react"
 import Header from "../LandingPage/components/Header"
-import ContactForm from '../LandingPage/components/ContactForm'
+import TextAndContact from "../LandingPage/components/TextAndContact"
 import { LayoutWrapper } from "./style"
+import Container from "../Container"
 
 const Layout = ({ language, children }) => {
   return (
     <LayoutWrapper>
-      <Header language={language}/>
-      
-      <ContactForm language={language} />
+      <Header language={language} />
+      <Container>
+        <TextAndContact />
+      </Container>
     </LayoutWrapper>
   )
 }
