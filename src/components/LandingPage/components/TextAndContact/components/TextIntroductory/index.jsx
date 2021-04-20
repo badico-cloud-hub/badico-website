782ec5e23790a806086bfe391bc94ce9a67754ae
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-20 13:38:27 -0300

    feat(components/landingpage/components/textandcontact): add text

---
+++ src/components/LandingPage/components/TextAndContact/components/TextIntroductory/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container } from "./style"
+
+const TextIntroductory = () => {
+  return (
+    <Container>
+      <h1>Esta difícil para encontrar um desenvolvedor para sua empresa?</h1>
+      <p>Com a gente você pode ter muito mais do que apenas um desenvolvedor</p>
+    </Container>
+  )
+}
+
+export default TextIntroductory
