d81d347d6dce24219b5c9969f1054b5ad1c995e2
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-20 16:52:14 -0300

    feat(components/landingpage/components/beneficies): add new component

---
+++ src/components/LandingPage/components/Beneficies/index.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import * as Styled from "./style"
+
+import Decrease from "../../images/income.svg"
+import Increase from "../../images/increase.svg"
+import Diversification from "../../images/diversification.svg"
+
+const Beneficies = () => {
+  return (
+    <Styled.Container>
+      <Styled.Box>
+        <Styled.Opportunities>
+          <Styled.AlignOpportunities>
+            <Styled.MiniBox>
+              <Decrease className="decrease" />
+              <Styled.Title>Diminuir custo</Styled.Title>
+              <Styled.Text>
+                Lorem ipsum nisi facilisis torquent suspendisse non, quis
+                lobortis interdum ante conubia.
+              </Styled.Text>
+            </Styled.MiniBox>
+            <Styled.MiniBox>
+              <Increase className="increase" />
+              <Styled.Title>Aumentar renda</Styled.Title>
+              <Styled.Text>
+                Lorem ipsum nisi facilisis torquent suspendisse non, quis
+                lobortis interdum ante conubia.
+              </Styled.Text>
+            </Styled.MiniBox>
+          </Styled.AlignOpportunities>
+          <Styled.MiniBox>
+            <Diversification className="diversification" />
+            <Styled.Title>Diminuir riscos</Styled.Title>
+            <Styled.Text>
+              Lorem ipsum nisi facilisis torquent suspendisse non, quis lobortis
+              interdum ante conubia.
+            </Styled.Text>
+          </Styled.MiniBox>
+        </Styled.Opportunities>
+        <Styled.Button>Quero ter um time incrível</Styled.Button>
+      </Styled.Box>
+      <Styled.Background />
+    </Styled.Container>
+  )
+}
+
+export default Beneficies
