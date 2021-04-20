9e278f5586ba05f09d655e0b93084944c85b554d
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-20 14:20:44 -0300

    feat(components/layoutlandingpage): add teamcomplete component

--- src/components/LayoutLandingPage/index.jsx
+++ src/components/LayoutLandingPage/index.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react"
+import Container from "../Container"
 import Header from "../LandingPage/components/Header"
 import TextAndContact from "../LandingPage/components/TextAndContact"
+import TeamComplete from '../LandingPage/components/TeamComplete'
 import { LayoutWrapper } from "./style"
-import Container from "../Container"
 
 const Layout = ({ language, children }) => {
   return (
@@ -10,6 +11,7 @@ const Layout = ({ language, children }) => {
       <Header language={language} />
       <Container>
         <TextAndContact />
+        <TeamComplete />
       </Container>
     </LayoutWrapper>
   )
