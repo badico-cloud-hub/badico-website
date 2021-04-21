e3306f9f0af690cce6e031aa374d4617ce70aabc
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-21 13:05:45 -0300

    chore(components/layoutlandingpage): remove import for on-demand

--- src/components/LayoutLandingPage/index.jsx
+++ src/components/LayoutLandingPage/index.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react"
 import Container from "../Container"
 import Header from "../LandingPage/components/Header"
-import TextAndContact from "../LandingPage/components/TextAndContact"
-import TeamComplete from '../LandingPage/components/TeamComplete'
-import Beneficies from '../LandingPage/components/Beneficies'
 import { LayoutWrapper } from "./style"
 
 const Layout = ({ language, children }) => {
@@ -11,9 +8,7 @@ const Layout = ({ language, children }) => {
     <LayoutWrapper>
       <Header language={language} />
       <Container>
-        <TextAndContact />
-        <TeamComplete />
-        <Beneficies />
+        <main>{children}</main>
       </Container>
     </LayoutWrapper>
   )
