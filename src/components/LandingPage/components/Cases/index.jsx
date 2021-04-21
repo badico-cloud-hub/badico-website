aa5e13e2e87ba3f84497fe84a4eb25e6bbbb05ab
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-21 13:55:25 -0300

    feat(components/landingpage/components/cases): created cases component

---
+++ src/components/LandingPage/components/Cases/index.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import * as Styled from "./style"
+import Gallery from '../../../Gallery'
+
+const Cases = () => {
+  return (
+    <Styled.Container>
+      <Styled.Title>Success cases</Styled.Title>
+      <Styled.Text>
+        Lorem ipsum scelerisque vel ac convallis nisl vestibulum facilisis elit,
+        aptent varius vestibulum primis taciti vehicula tristique donec eu
+        litora, congue laoreet pretium interdum id condimentum pulvinar
+        venenatis. sit dui lobortis rhoncus varius convallis elementum convallis
+        aliquam mi diam, curae ullamcorper imperdiet ac etiam nostra donec
+      </Styled.Text>
+    <Gallery />
+    <Styled.Button>Quero ter um time incrível</Styled.Button>
+    </Styled.Container>
+  )
+}
+
+export default Cases
