7d3943074d39b83fc3e15db7395d99b8aaed6a4f
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-21 13:05:16 -0300

    chore(components/layout): rename file

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -7,18 +7,10 @@ import Bg from "../../images/bgDesktop.svg"
 
 import { LayoutWrapper } from "./style"
 
-// import { useIntl, changeLocale } from "gatsby-plugin-intl"
-
 const Layout = ({ language, children }) => {
-  // const locale = useIntl().locale
-
-  // useEffect(() => {
-  //   changeLocale(locale)
-  // }, [locale])
-
   return (
     <LayoutWrapper>
-      <Header language={language}/>
+      <Header language={language} />
       <Bg fill="#0e273b" className="bgWave" />
       <Container>
         <main>{children}</main>
