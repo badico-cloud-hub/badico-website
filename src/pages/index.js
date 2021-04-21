7acb649f51dcb3995540965399c292bb5335a889
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-21 13:06:35 -0300

    feat(pages/index): updated import components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,37 @@
 import React from "react"
 import LanguageProvider from "../providers/LanguageProvider"
-import MainPage from '../components/Main'
-import { WebsiteProvider } from '../contexts'
-
-
-const language = "en"
+import Layout from "../components/Layout/"
+import SEO from "../components/seo"
+import HeroLayout from "../components/HeroLayout"
+import Companies from "../components/Companies"
+import FloatingButton from "../components/FloatingButton"
+import CraftmanshipLayout from "../components/CraftmanshipLayout/index"
+import TechStacks from "../components/TechStacks"
+import Capacities from "../components/Capacities"
+import Cases from "../components/Cases"
+import About from "../components/About"
+import { WebsiteProvider } from "../contexts"
 
 const IndexPage = () => {
-  
+  const language = "en"
+
   return (
     <WebsiteProvider>
-      <LanguageProvider >
-        <MainPage language={language}/>
+      <LanguageProvider>
+        <Layout language={language}>
+          <SEO title="Badico Cloud Engineering | We are you IT departament" />
+          <HeroLayout language={language} />
+          <FloatingButton language={language} />
+          <Companies language={language} />
+          <CraftmanshipLayout language={language} />
+          <TechStacks language={language} />
+          <Capacities language={language} />
+          <Cases language={language} />
+          <About language={language} />
+        </Layout>
       </LanguageProvider>
     </WebsiteProvider>
-  )}
+  )
+}
 
 export default IndexPage
