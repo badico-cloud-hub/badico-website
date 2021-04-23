00cade2aa12fe3ce505fada7d5e33fc2e8179f70
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-23 17:10:41 -0300

    refac(components/header): comment lines and add valid route

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react"
 import Logobadico from "../../images/logo.svg"
 import * as typeformEmbed from "@typeform/embed"
-import Typeform from "../Typeform/index"
+// import Typeform from "../Typeform/index"
 import MenuIcon from "../../images/menuwhite.svg"
 import Close from "../../images/close.svg"
 import * as Styled from "./style"
-import lang from '../../intl/langs';
+import lang from "../../intl/langs"
 
-  
 const Header = ({ language }) => {
   const [form, setForm] = useState(null)
   const [isOpen, setIsOpen] = useState(false)
@@ -49,7 +48,7 @@ const Header = ({ language }) => {
           <nav class="drop-content">
             <ul>
               <li>
-                <a href="#">Home</a>
+                <a href={"https://badico.cloud/"}>Home</a>
               </li>
               {/* <li>
               <a href="#">Our Process</a>
@@ -57,35 +56,37 @@ const Header = ({ language }) => {
               {/* <li>
               <a href="#">Meet Our Time</a>
             </li> */}
-            <li>
-              <a href="#capacities">{lang[language]["menu.div.options.1"]}</a>
-            </li>
-            {/* <li>
+              <li>
+                <a href="#capacities">{lang[language]["menu.div.options.1"]}</a>
+              </li>
+              {/* <li>
               <a href="#">Blog</a>
             </li>
              */}
-            <li>
-              <a href="#about">{lang[language]["menu.div.options.4"]}</a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  form.open()
-                }}
-              >
-                {lang[language]["menu.div.options.5"]}</a>
-            </li>
-          </ul>
-        </nav>
-        <button
-          className="close"
-          onClick={() => {
-            setIsOpen(false)
-          }}
-        >
-          <Close />
-        </button>
-      </div>
+              <li>
+                <a href="#about">{lang[language]["menu.div.options.4"]}</a>
+              </li>
+              <li>
+                <a
+                  href={"https://badico.cloud/"}
+                  onClick={() => {
+                    form.open()
+                  }}
+                >
+                  {lang[language]["menu.div.options.5"]}
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <button
+            className="close"
+            onClick={() => {
+              setIsOpen(false)
+            }}
+          >
+            <Close />
+          </button>
+        </div>
       </Styled.Container>
     </Styled.HeaderWrapper>
   )
