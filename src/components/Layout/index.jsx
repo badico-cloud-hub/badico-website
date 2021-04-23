984908db9ae35d7b49bfa2a18b7be440bcdb725b
Antonio Lucio <antonioluciofb@hotmail.com>
2021-04-23 17:12:25 -0300

    refac(components/layout): commented lines

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import Header from "../Header"
 import Footer from "../Footer"
-import Container from "../Container"
+// import Container from "../Container"
 
 import { LayoutWrapper } from "./style"
 
