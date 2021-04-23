bb22de0b162f0e0cb6d2f30c4b64c2bec4e15e7a
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-23 11:19:04 -0300

    feat(components/calendly): add children props text

--- src/components/Calendly/index.jsx
+++ src/components/Calendly/index.jsx
@@ -2,15 +2,14 @@ import React, { Component } from "react"
 import { openPopupWidget } from "react-calendly"
 import Calendar from "../../images/calendar.svg"
 import * as Styled from "./style"
-import lang from "../../intl/langs"
 
-const Calendly = ({ url, language }) => {
+const Calendly = ({ children }) => {
   const onClick = () => openPopupWidget({ url: "https://calendly.com/badico" })
 
   return (
     <>
       <Styled.Button onClick={onClick}>
-        {lang[language]["hero.buttons.1"]}
+        {children}
         <span /> <span /> <span /> <span />
       </Styled.Button>
     </>
