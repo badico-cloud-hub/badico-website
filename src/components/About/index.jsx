ed53c9a1cb1dc7aa32fa2938baca7183d898184a
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-27 15:23:42 -0300

    feat(components/about): adjust style section about us

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,22 +1,20 @@
 import React from "react"
 import Container from "../Container/index"
-import * as S from "./styled"
+import * as Styled from "./styled"
 import lang from "../../intl/langs"
 
 const About = ({ language }) => {
   return (
-    <S.AboutUs>
-      <Container>
-        <h1>{lang[language]["about.h1"]}</h1>
-        <h2>{lang[language]["about.h2"]}</h2>
-        <S.Text class="text-about" width="92.5%">
+    <Styled.AboutUs>
+        <Styled.Title>{lang[language]["about.h1"]}</Styled.Title>
+        <Styled.SubTitle>{lang[language]["about.h2"]}</Styled.SubTitle>
+        <Styled.Text class="text-about" width="92.5%">
           {lang[language]["about.text.0"]}
-        </S.Text>
-        <S.Text class="text-about" width="90%">
+        </Styled.Text>
+        <Styled.Text class="text-about" width="90%">
           {lang[language]["about.text.1"]}
-        </S.Text>
-      </Container>
-    </S.AboutUs>
+        </Styled.Text>
+    </Styled.AboutUs>
   )
 }
 
--- src/components/About/styled.js
+++ src/components/About/styled.js
@@ -1,90 +1,135 @@
 import styled from "styled-components"
 import media from "styled-media-query"
 
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 1920px;
+`
+
 export const AboutUs = styled.div`
-  background: #ffffff;
-  margin: 7rem 2rem 2rem 2rem;
-  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  margin: 64px auto 76px;
+  width: 80%;
+  align-items: center;
 
-  h1 {
-    font-size: 1.3rem;
-    /* font-weight: 600; */
-    color: #02293F;
-    margin-bottom: 1.5rem;
-    text-align: center;
+  @media (min-width: 768px) {
+    width: 78%;
   }
 
-  h2 {
-    color: #27DEBF;
-    width: 35%;
-    position: relative;
-    font-size: 1rem;
-    text-align: center;
-    font-weight: 600;
-    margin: 0 auto 2rem auto;
+  @media (min-width: 1366px) {
+    width: 1086px;
+    align-items: initial;
   }
 
-  h2:after {
+  @media (min-width: 1920px) {
+    width: 1492px;
+    margin: 124px auto 104px;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 600;
+  color: #0e273b;
+  text-align: center;
+  margin-bottom: 24px;
+
+  @media (min-width: 428px) {
+    margin-bottom: 16px;
+  }
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 56px;
+  }
+
+  @media (min-width: 1366px) {
+    margin-bottom: 32px;
+  }
+
+  @media (min-width: 1920px) {
+    font-size: 34px;
+    line-height: 41px;
+  }
+`
+
+export const SubTitle = styled.h2`
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  line-height: 16px;
+  font-weight: 600;
+  text-align: center;
+  color: #27debf;
+  text-transform: uppercase;
+  margin-bottom: 38px;
+  letter-spacing: 0.02px;
+
+  :after {
     content: "";
     display: inline-block;
-    background: rgba(15, 191, 97, 0.1);
-    width: 100%;
-    height: 0.5rem;
+    background: #0fbf61;
+    opacity: 0.1;
+    width: 90px;
+    height: 7px;
     border-radius: 5px;
-    position: absolute;
-    left: 0;
-    top: 100%;
-    margin-top: 0.9rem;
+    margin-top: 8px;
   }
 
-  .text-about{
-    letter-spacing: -0.5px;
+  @media (min-width: 428px) {
+    font-size: 16px;
+    line-height: 18px;
+    margin-bottom: 42px;
+
+    :after {
+      width: 110px;
+    }
   }
 
-  ${media.greaterThan("768px")`
-  h1 {
-      margin-left: 0.3rem;
-      margin-bottom: 2.5rem;
+  @media (min-width: 768px) {
+    font-size: 20px;
+    line-height: 24px;
+    margin-bottom: 56px;
+
+    :after {
+      width: 120px;
+    }
   }
-  h2 {
+
+  @media (min-width: 1366px) {
     text-align: left;
-    margin-left: 1.5rem;
-    margin-bottom: 2.8rem;
+    text-transform: none;
+    margin-bottom: 64px;
+
+    :after {
+      width: 178px;
+      height: 15px;
+    }
   }
 
-  h2:after {
-    width: 9.5rem;
-    height: 0.7rem;
-    left: 0;
-    top: 100%;
+  @media (min-width: 1920px) {
+    font-size: 24px;
+    line-height: 29px;
   }
-
-  p {
-    font-weight: 400;
-    margin-left: 1.5rem;
-    font-size: 1rem;
-    color: #151d41;
-  }
-    `}
-
-  ${media.greaterThan("950px")`
-
-  h2 {
-    margin-left: 2rem;
-  }
-
-  p {
-    margin-left: 2rem;
-  }
-    `}
-
-    ${media.greaterThan("1200px")`
-    padding: 3rem;
-    `}
 `
+
 export const Text = styled.p`
-  font-weight: 500;
-  font-size: 0.875rem;
-  width: ${props => props.width};
-  color: #151d41;
+  font-size: 14px;
+  color: #52575c;
+  line-height: 17px;
+  margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+    line-height: 24px;
+  }
+
+  @media (min-width: 1366px) {
+    font-size: 20px;
+    line-height: 24px;
+  }
 `
