a62eb9600f2598f847611f2a0b5b87cce5dde2a6
WelGomws <wellingtond.gomes@hotmail.com>
2021-04-28 11:31:07 -0300

    fix(pages/404): fix build failed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,17 @@
 import React from "react"
 
-import Layout from "../components/Layout/"
+// import Layout from "../components/Layout/"
 import SEO from "../components/seo"
 
 
 const NotFoundPage = ({ language }) => (
-  <Layout language={language}>
+  // <Layout language={language}>
     <>
       <SEO title="404: Not found" />
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </>
-  </Layout>
+  // </Layout>
 )
 
 export default NotFoundPage
