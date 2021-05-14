2c729ad12030d6e64f5b72cdeee448f0487fea27
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-14 09:03:44 -0300

    feat(components/form): add tag form

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,7 @@
+            <DivForm>
+                <NameInput type="text" placeholder="Name*"></NameInput>
+                <EmailInput type="email" placeholder="Email*"></EmailInput>
+                <TextInput type="text" placeholder="Message*"></TextInput>
+                <EmailContactButton type="button">Contact e-mail</EmailContactButton>
+                <WhatsappContactButton type="button">Contact Whatsapp</WhatsappContactButton>
+            </DivForm>
