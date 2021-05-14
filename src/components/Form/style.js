4a0883e9986ac9dd8150f903112980b2b45574df
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2021-05-14 17:16:45 -0300

    feat(component/form):add media queries and new component

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -4,27 +4,104 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
-    padding-left: 30px;
     padding-bottom: 70px;
+
+    @media(min-width: 1366px){
+        flex-direction: row;
+        height: 642px;
+    }
+
 `
+
+export const DivTelemarketing = styled.div`
+    display: none;
+    
+    @media(min-width: 1366px){
+        width: 50%;
+        height: 100%;
+        display: flex;
+        justify-content:center;
+        align-items: center;
+        
+        svg{
+            width: 386px;
+            height: 394px;
+        }
+    }
+`
+export const DivContact = styled.div`
+    @media(min-width: 1366px){
+        align-items: center;
+        width: 50%;
+    }
+`
+
 export const TitleContact = styled.div`
     font: normal normal medium 20px/24px Roboto;
+    background: white;
     color: #0E273B;
     width: 86px;
     height: 24px;
     margin-bottom: 10px;
+    font-weight: bold;
+
+    @media (min-width: 768px){
+        width: 103px;
+        height: 29px;
+        font: normal normal normal 24px/29px Roboto;
+    }
+
+    @media (min-width: 1366px){
+        width: 197px;
+        height: 33px;
+        font: normal normal normal 24px/29px Roboto;
+        font-weight: bold;
+    }
+
+    @media (min-width: 1920px){
+        width: 197px;
+        height: 36px;
+        font: normal normal normal 34px/41px Roboto;
+    }
+    
 `
 export const TextContact = styled.div`
     font: normal normal normal 16px/24px Roboto;
     color: #0E273B;
     width: 286px;
     height: 45px;
-    margin-bottom: 10px;
+    margin-bottom: 8px;
+
+    @media (min-width: 768px){
+        width: 598px;
+        height: 79px;
+    }
+
+    @media (min-width: 1366px){
+        width: 588px;
+        height: 51px;
+        font: normal normal normal 20px/24px Roboto;
+    }
+
+    @media (min-width: 1920px){
+        width: 767px;
+        height: 26px;
+    }
 `
 
 export const DivForm = styled.form`
     display: flex;
     flex-direction: column;
+
+    @media (min-width: 768px){
+        margin-top:-20px;
+    }
+
+    @media (min-width: 1366px){
+        margin-top:30px;
+    }
+
+    
 `
 
 export const NameInput = styled.input`
@@ -34,7 +111,22 @@ export const NameInput = styled.input`
     border: 2px solid #E8E8E8;
     border-radius: 2px;
     margin-bottom: 3px;
-    opacity: 1;
+
+    @media (min-width: 768px){
+        width: 598px;
+        height: 69px;
+    }
+
+    @media (min-width: 1366px){
+        width: 588px;
+        height: 72px;
+    }
+
+    @media (min-width: 1920px){
+        width: 413px;
+        height: 73px;
+        margin-rigth:auto;
+    }
 `
 
 export const EmailInput = styled.input`
@@ -44,7 +136,23 @@ export const EmailInput = styled.input`
     border: 2px solid #E8E8E8;
     border-radius: 2px;
     margin-bottom: 3px;
-    opacity: 1;
+
+    @media (min-width: 768px){
+        width: 598px;
+        height: 69px;
+    }
+
+    @media (min-width: 1366px){
+        width: 588px;
+        height: 72px;
+    }
+
+    @media (min-width: 1920px){
+        width: 415px;
+        height: 73px;
+        margin-left:auto;
+        margin-top: -70px;
+    }
 `
 
 export const TextInput = styled.textarea`
@@ -54,6 +162,21 @@ export const TextInput = styled.textarea`
     border: 2px solid #E8E8E8;
     border-radius: 2px;
     margin-bottom: 10px;
+
+    @media (min-width: 768px){
+        width: 598px;
+        height: 144px;
+    }
+
+    @media (min-width: 1366px){
+        width: 588px;
+        height: 229px;
+    }
+
+    @media (min-width: 1920px){
+        width: 852px;
+        height: 229px;
+    }
 `
 
 export const EmailContactButton = styled.button`
@@ -64,6 +187,24 @@ export const EmailContactButton = styled.button`
     color: #0E273B;
     font: normal normal normal 14px/17px Roboto;
     margin-right: 150px;
+    background: white;
+
+    @media (min-width: 768px){
+        width: 253px;
+        height: 57px;
+        font-weight: bold;
+        font: normal normal medium 20px/24px Roboto;
+        margin-top:30px;
+        margin-right:auto;
+        margin-left: 20px;
+    }
+
+    @media (min-width: 1366px){
+        width: 188px;
+        height: 48px;
+        font: normal normal medium 16px/24px Roboto;
+        margin-right:auto;
+    }
 `
 
 export const WhatsappContactButton = styled.button`
@@ -75,4 +216,23 @@ export const WhatsappContactButton = styled.button`
     margin-left: 150px;
     font: normal normal normal 14px/17px Roboto;
     margin-top: -34.5px;
+    background: white;
+
+    @media (min-width: 768px){
+        width: 253px;
+        height: 57px;
+        font-weight: bold;
+        font: normal normal medium 20px/24px Roboto;
+        margin-left:auto;
+        margin-right: 20px;
+        margin-top: -57px;
+    }
+
+    @media (min-width: 1366px){
+        width: 188px;
+        height: 48px;
+        font: normal normal medium 16px/24px Roboto;
+        margin-left:230px;
+        margin-top:-48px
+    }
 `
