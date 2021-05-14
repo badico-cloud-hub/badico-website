f3394ab2c7d9fd86c09d1d095e3dd1ef42b6df0c
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2021-05-14 17:15:34 -0300

    feat(components/form): add components and svg image Co-authored-by: Edson Almeida Silva Júnior <EdJunior0@users.noreply.github.com>

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,22 +1,40 @@
 import React from "react"
-import {Container, TitleContact, TextContact, DivForm, NameInput, EmailInput, TextInput, EmailContactButton, WhatsappContactButton} from "./style"
+import {
+    Container,
+    TitleContact,
+    TextContact,
+    DivForm,
+    NameInput,
+    EmailInput,
+    TextInput,
+    EmailContactButton,
+    WhatsappContactButton,
+    DivTelemarketing,
+    DivContact
+} from "./style"
+import Telemarketing from "../../images/telemarketing.svg"
 
 const Form = () => {
     return (
         <Container>
-            <TitleContact>
-                <p>Talk to us</p>
-            </TitleContact>
-            <TextContact>
-                <p>Lorem ipsum dolor sit amet, consectetur pretium </p>
-            </TextContact>
-            <DivForm>
-                <NameInput type="text" placeholder="Name*"></NameInput>
-                <EmailInput type="email" placeholder="Email*"></EmailInput>
-                <TextInput type="text" placeholder="Message*"></TextInput>
-                <EmailContactButton type="button">Contact e-mail</EmailContactButton>
-                <WhatsappContactButton type="button">Contact Whatsapp</WhatsappContactButton>
-            </DivForm>
+            <DivTelemarketing>
+                <Telemarketing />
+            </DivTelemarketing>
+            <DivContact>
+                <TitleContact>
+                    <p>Talk to us</p>
+                </TitleContact>
+                <TextContact>
+                    <p>Lorem ipsum dolor sit amet, consectetur pretium pretium tempor.Lorem ipsum dolor </p>
+                </TextContact>
+                <DivForm>
+                    <NameInput type="text" placeholder="Name*"></NameInput>
+                    <EmailInput type="email" placeholder="Email*"></EmailInput>
+                    <TextInput type="text" placeholder="Message*"></TextInput>
+                    <EmailContactButton type="button">Contact e-mail</EmailContactButton>
+                    <WhatsappContactButton type="button">Contact Whatsapp</WhatsappContactButton>
+                </DivForm>
+            </DivContact>
         </Container>
     )
 }
