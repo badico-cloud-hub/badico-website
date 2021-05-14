2a08e6f8212181db2678695ef7d6cbe9bce8d4bd
ydrouulis <lombardi.droulis@gmail.com>
2021-05-14 13:32:30 -0300

    refac(components/processlayout): refac querries

--- src/components/ProcessLayout/ProcessWrapper.js
+++ src/components/ProcessLayout/ProcessWrapper.js
@@ -2,42 +2,35 @@ import styled from "styled-components"
 import media from "styled-media-query"
 
 export const ProcessWrapper = styled.div`
-  font-family: GalanoGrotesque;
-  
+  font-family: Roboto;
 
-  .prodImg{
+  .prodImg {
     display: none;
   }
 
   ${media.greaterThan("768px")`
-  margin-top: 6rem;
-  position: relative;
-
-  .prodImg{
-    display: block;
-    width: 35%;
-    height: 300px;
-    position: absolute;
-    left: 60%;
-    top: -100px;
-  }
+    width: 592px;
+    height: 972px;
+    margin: 0 auto;
   `}
 
-  ${media.greaterThan("1024px")`
-
+  ${media.greaterThan("1366px")`
   .prodImg{
-    left: 55%;
-    top: -150px;
-    width: 39%;
-    height: 500px;
+    display: inline-block;
+    width: 555px;
+    float: right;
+    position: relative;
+    left: 300px;
+    bottom: 300px;
   }
   `}
 `
 
 export const Title = styled.h1`
   font-size: 1.5rem;
-  margin-left: 2rem;
-  margin-bottom: 3.5rem;
+  margin: 6rem auto 3.5rem auto;
+  display: flex;
+  justify-content: center;
 
   ${media.greaterThan("768px")`
   margin-bottom: 6rem;
@@ -48,182 +41,192 @@ export const Steps = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
-  
 
-  .stepsDesktop{
+  .stepsDesktop {
     display: none;
   }
 
   ${media.greaterThan("768px")`
   display: flex;
   height: 500px;
-  .stepsDesktop{
-    display: inline-block;
-    width: 500px;
-    height: 90%;
-    position: absolute;
-    left: -45px;
-  }
   `}
 
-  ${media.greaterThan("1024px")`
+  ${media.greaterThan("1366px")`
   .stepsDesktop{
-    width: 550px;
-    height: 110%;
+    display: block;
+    width: 660px;
+    height: 886px;
     left: -15px;
+    position: absolute;
+    left: -280px;
   }
   `}
 `
 
-export const Cards =  styled.div`
+export const Cards = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-  width: 95%;
-  max-width: 460px;
+  width: 320px;
   overflow: hidden;
-  padding-top: .9rem;
+  padding-top: 0.9rem;
 
-  .no-reverse{
+  .no-reverse {
     flex-direction: row;
     margin-left: 1rem;
   }
-  
-  .reverse{
+
+  .reverse {
     flex-direction: row-reverse;
     padding-right: 1.8rem;
   }
 
   ${media.greaterThan("768px")`
-    margin: 0;
-    width: 300px;
     overflow: visible;
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    .card1{
-      position: absolute;
-      top: 0px;
-      left: 220px;
-    }
-
-    .card2{
-      position: absolute;
-      top: 115px;
-      left: 300px;
-    }
-
-    .card3{
-      position: absolute;
-      top: 230px;
-      left: 340px;
-    }
-
-    .card4{
-      position: absolute;
-      top: 340px;
-      left: 420px;
-    }
+    margin: 0;
+    width: 100%;
   `}
 
-  ${media.greaterThan("1024px")`
-
-    .card1{
-      top: 10px;
-      left: 250px;
-    }
-
-    .card2{
-      position: absolute;
-      top: 140px;
-      left: 350px;
-    }
-
-    .card3{
-      position: absolute;
-      top: 280px;
-      left: 430px;
-    }
-
-    .card4{
-      position: absolute;
-      top: 415px;
-      left: 500px;
-    }
+  ${media.greaterThan("1366px")`
+  
   `}
-` 
+`
 
 export const Card = styled.div`
-width: 100%;
-margin-bottom: 2.5rem; 
-display: flex;
-justify-content: space-around;
+  width: 100%;
+  height: 84px;
+  margin-bottom: 2.5rem;
+  display: flex;
+  justify-content: space-around;
 
-.step1{
-  position: relative;
-  left: -10px;
-}
+  .none {
+    width: 100%;
+    height: 100%;
+  }
 
-.content1{
-  position: relative;
-  left: -22px;
-}
+  .containerStep {
+    width: 126px;
+    height: 84px;
+  }
 
-.content3{
-  position: relative;
-  left: -21px;
-}
+  .content1 {
+    position: relative;
+    left: -22px;
+  }
 
+  .content3 {
+    position: relative;
+    left: -21px;
+  }
 
+  ${media.greaterThan("768px")`
+  width: 100%;
+  height: 159px;
+  border: 0;
+  margin-bottom: 87px;
 
-${media.greaterThan("768px")`
-width: 90%;
-.none{
-  display: none;
-}
+  .containerStep{
+    width: 239px;
+    height: 160px;
+  }
+  .step1{
+    width: 100%;
+    height: 100%;
+  }
+  .none{
+    display: block;
+    width: 239px;
+    height: 160px;
+  }
 `}
 
-${media.greaterThan("1024px")`
-height: 130px;
-width: 300px;
+${media.greaterThan("1366px")`
+  height: 167px;
+  width: 377px;
+
+  .none{
+    display: none;
+  }
+
+  .containerStep{
+    display: none;
+  }
+
+  .content1{
+    position: relative;
+    left: 50px;
+    top: 30px;
+  }
+
+  .content2{
+    position: relative;
+    left: 200px;
+    bottom: 10px;
+  }
+
+  .content3{
+    position: relative;
+    left: 290px;
+    bottom: 60px;
+  }
+
+  .content4{
+    position: relative;
+    left: 420px;
+    bottom: 100px;
+  }
 `}
 `
 
 export const Content = styled.div`
-background: #fff;
-padding: .5rem;
-width: 50%;
-box-shadow: 4px 7px 6px #0000003D;
-position: relative;
+  background: #fff;
+  padding: 0.5rem;
+  width: 146px;
+  height: 89px;
+  box-shadow: 4px 7px 6px #0000003d;
+  position: relative;
 
-.icon{
-  width: 30px;
-  height: 30px;
-  position: absolute;
-  top: -13px;
-  right: 12px;
-}
+  .icon {
+    width: 35px;
+    height: 35px;
+    position: absolute;
+    top: -13px;
+    right: 12px;
+    border-radius: 50%;
+    box-shadow: 0px 3px 6px #00000029;
+  }
 
-h2{
-    font-size: .9rem;
+  h2 {
+    font-size: 0.875rem;
     font-weight: 600;
     color: #404040;
-}
+  }
 
-p{
-    font-size: 0.8rem;
+  p {
+    font-size: 0.75rem;
     color: #808080;
-}
+  }
 
-${media.greaterThan("768px")`
-  width: 250px;
-  height: 90px;
-  
+  ${media.greaterThan("768px")`
+  width: 278px;
+  height: 170px;
+
+  .icon {
+    width: 56px;
+    height: 56px;
+  }
+
+  h2{
+    font-size: 1.25rem;
+  }
+
+  p{
+    font-size: 1rem;
+  }
 `}
 
-${media.greaterThan("1024px")`
-height: 110px;
-width: 100%;
+${media.greaterThan("1366px")`
+  height: 167px;
+  width: 100%;
 `}
 `
