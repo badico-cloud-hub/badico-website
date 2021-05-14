fe62fe7722d384aab03e9a205b78a66340251de8
ydrouulis <lombardi.droulis@gmail.com>
2021-05-14 13:31:52 -0300

    refac(components/processlayout): refac index

--- src/components/ProcessLayout/index.jsx
+++ src/components/ProcessLayout/index.jsx
@@ -1,5 +1,12 @@
 import React from "react"
-import { ProcessWrapper, Title, Steps, Content, Cards, Card } from "./ProcessWrapper"
+import {
+  ProcessWrapper,
+  Title,
+  Steps,
+  Content,
+  Cards,
+  Card,
+} from "./ProcessWrapper"
 import Container from "../Container/index"
 import PizzaIcon from "../../images/pizza-icon.svg"
 import SetIcon from "../../images/setting-icon.svg"
@@ -9,7 +16,7 @@ import Step1 from "../../images/mobile1.svg"
 import Step2 from "../../images/mobile2.svg"
 import Step3 from "../../images/mobile3.svg"
 import Step4 from "../../images/mobile4.svg"
-import Producao from "../../images/produção1.svg"
+import Producao from "../../images/producao1.svg"
 import StepsDesktop from "../../images/desktop.svg"
 
 const ProcessLayout = () => {
@@ -17,44 +24,52 @@ const ProcessLayout = () => {
     <ProcessWrapper>
       <Container>
         <Title>Our process</Title>
-        <Producao className="prodImg"/>
-        {/*<Producao/>*/}
+        {/* <Producao className="prodImg"/> */}
+        <Producao className="prodImg" />
         <Steps>
           <StepsDesktop className="stepsDesktop" />
           <Cards>
-          <Card className="no-reverse card1">
-            <Step1 className="step1 none" />
-            <Content className="content1">
-              <PizzaIcon className="icon" />
-              <h2>Lorem ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetuer</p>
-            </Content>
-          </Card>
-          <Card className="reverse card2">
-            <Step2 className="none" />
-            <Content className="content2">
-              <SetIcon className="icon" />
-              <h2>Lorem ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetuer</p>
-            </Content>
-          </Card>
-          <Card className="no-reverse card3">
-            <Step3 className="none" />
-            <Content className="content3">
-              <ToolIcon className="icon" />
-              <h2>Lorem ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetuer</p>
-            </Content>
-          </Card>
-          <Card className="reverse card4">
-            <Step4 className="none" />
-            <Content className="content4">
-              <AvatarIcon className="icon" />
-              <h2>Lorem ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetuer</p>
-            </Content>
-          </Card>
-          </Cards> 
+            <Card className="no-reverse card1">
+              <div className="containerStep">
+                <Step1 className="none" />
+              </div>
+              <Content className="content1">
+                <PizzaIcon className="icon" />
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+              </Content>
+            </Card>
+            <Card className="reverse card2">
+              <div className="containerStep">
+                <Step2 className="none" />
+              </div>
+              <Content className="content2">
+                <SetIcon className="icon" />
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+              </Content>
+            </Card>
+            <Card className="no-reverse card3">
+              <div className="containerStep">
+                <Step3 className="none" />
+              </div>
+              <Content className="content3">
+                <ToolIcon className="icon" />
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+              </Content>
+            </Card>
+            <Card className="reverse card4">
+              <div className="containerStep">
+                <Step4 className="none" />
+              </div>
+              <Content className="content4">
+                <AvatarIcon className="icon" />
+                <h2>Lorem ipsum</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+              </Content>
+            </Card>
+          </Cards>
         </Steps>
       </Container>
     </ProcessWrapper>
