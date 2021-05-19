e55f203fdfd9289f0a3046819cced6db32e06bb8
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-19 12:14:21 -0300

    feat(components/team/members/memberswrapper): add a new proprierties

--- src/components/Team/Members/MembersWrapper.js
+++ src/components/Team/Members/MembersWrapper.js
@@ -1,7 +1,9 @@
 import styled from "styled-components"
 
 export const MembersWrapper = styled.div`
+  width: 270px;
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-around;
   max-width: 1040px;
 `
