fd5a7c8598830f940729acaea541c2b6be4e99f2
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-19 08:59:31 -0300

    fix(pages): fix import path

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import LanguageProvider from "../providers/LanguageProvider"
 
 import Team from "../components/Team/index"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import HeroLayout from "../components/HeroLayout"
 import Companies from "../components/Companies"
