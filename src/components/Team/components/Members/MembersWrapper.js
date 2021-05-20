e0b3cb2b90eb3d13d1c671e88d0eb9f0557451b0
ydrouulis <lombardi.droulis@gmail.com>
2021-05-20 18:21:00 -0300

    feat(components/team/members): add new media query

--- src/components/Team/components/Members/MembersWrapper.js
+++ src/components/Team/components/Members/MembersWrapper.js
@@ -1,12 +1,17 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const MembersWrapper = styled.div`
   width: 270px;
   height: ${props => props.height};
   overflow: ${props => props.overflow};
-  transition: height 0.4s linear;
+  transition: height 1s linear;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   max-width: 1040px;
+
+  ${media.greaterThan("768px")`
+  width: 590px;
+  `}
 `
