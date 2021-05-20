c2b7b0f4a674a2a3a727f2135235456f998766d2
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-20 12:02:00 -0300

    feat(components/team/members): add props in proprieties

--- src/components/Team/Members/MembersWrapper.js
+++ src/components/Team/Members/MembersWrapper.js
@@ -2,6 +2,9 @@ import styled from "styled-components"
 
 export const MembersWrapper = styled.div`
   width: 270px;
+  height: ${props => props.height};
+  overflow: ${props => props.overflow};
+  transition: height 0.4s linear;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
