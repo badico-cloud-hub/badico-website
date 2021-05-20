8baaa8081c7e9e72dd2fbbf0d29734cb20a66ccc
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-20 11:59:42 -0300

    feat(components/team): create functions to control states

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,8 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
 import Members from "./Members/index"
-import { TeamWrapper, Container, Title, Description } from "./TeamWrapper"
+import Arrow from "../../images/arrow_down.svg"
+import { TeamWrapper, Container, Title, Description, ButtonDrop } from "./TeamWrapper"
 
 const Team = () => {
+  const [down, setDown] = useState(false)
+  const [height, setHeight] = useState({
+    mobile: "336px"
+  })
+  const [rotate, setRotate] = useState("rotate(0)")
+
+  const dropDown = () => {
+    console.log(down)
+    setDown(!down)
+
+    setHeight( (prev) => {
+      if(prev.mobile == "504px"){
+        return {
+          mobile: "336px"
+        }
+      } else {
+        return {
+          mobile: "504px"
+        }
+      }
+    })
+
+    setRotate( (prev) => {
+      if( prev == "rotate(180deg)"){
+        return "rotate(0)"
+      } else {
+        return "rotate(180deg)"
+      }
+    })
+  }
+
   return (
     <TeamWrapper>
       <Container>
@@ -11,8 +43,10 @@ const Team = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
         </Description>
       </Container>
-
-      <Members/>
+      <Members down={down} height={height}/>
+      <ButtonDrop onClick={dropDown} rotate={rotate}>
+        <Arrow/>
+      </ButtonDrop>
     </TeamWrapper>
   )
 }
