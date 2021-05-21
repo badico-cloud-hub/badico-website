a0838f8a92b529c79d526cc284b4ffcec02881d2
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-21 10:53:45 -0300

    feat(components/team/members): add media query

--- src/components/Team/components/Members/MembersWrapper.js
+++ src/components/Team/components/Members/MembersWrapper.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import media from "styled-media-query"
 
 export const MembersWrapper = styled.div`
-  width: 270px;
+  width: 100%;
   height: ${props => props.height};
   overflow: ${props => props.overflow};
   transition: height 1s linear;
@@ -10,8 +10,4 @@ export const MembersWrapper = styled.div`
   flex-wrap: wrap;
   justify-content: space-around;
   max-width: 1040px;
-
-  ${media.greaterThan("768px")`
-  width: 590px;
-  `}
 `
