740427488fe478b62670a4ed9f762105f330eff7
EdJunior0 <edsonjunior.1219@gmail.com>
2021-05-21 11:12:00 -0300

    feat(components/team/members): change time transition

--- src/components/Team/components/Members/MembersWrapper.js
+++ src/components/Team/components/Members/MembersWrapper.js
@@ -5,7 +5,7 @@ export const MembersWrapper = styled.div`
   width: 100%;
   height: ${props => props.height};
   overflow: ${props => props.overflow};
-  transition: height 1s linear;
+  transition: height 0.8s linear;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
