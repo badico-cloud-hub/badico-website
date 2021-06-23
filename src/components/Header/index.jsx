bc9a766a990785d3027e6cace7ea9197aced6d26
ydrouulis <lombardi.droulis@gmail.com>
2021-06-23 16:50:19 -0300

    feat(components/header) add navdasktop component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,17 +3,19 @@ import Logobadico from "../../images/logo.svg"
 import * as Styled from "./style"
 import lang from "../../intl/langs"
 import Menudrop from './Menudrop';
+import NavDesktop from './NavDesktop'
 
 const Header = ({ language }) => {
 
   return (
-    <Styled.HeaderWrapper>
+  <Styled.HeaderWrapper>
       <Styled.Container>
         <Styled.Ghost />
         <Styled.BoxLogo>
           <Logobadico className="logo" alt="Badico's logo" />
         </Styled.BoxLogo>
         <Menudrop lang={lang} language={language} />
+        <NavDesktop/>
       </Styled.Container>
     </Styled.HeaderWrapper>
   )
