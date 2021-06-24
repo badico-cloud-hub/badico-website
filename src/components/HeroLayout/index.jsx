cdce39f01df9cfafbd2d3684295b79865a17fb57
ydrouulis <lombardi.droulis@gmail.com>
2021-06-24 13:09:01 -0300

    refac(components/herolayout): refac herolayout

--- src/components/HeroLayout/index.jsx
+++ src/components/HeroLayout/index.jsx
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from "react"
-import { openPopupWidget } from "react-calendly"
-import Calendly from "../Calendly"
-import Typeform from '../Typeform'
+import React from "react"
+import CalendlyButton from "../CalendlyButton"
+import TypeformButton from '../TypeformButton'
 import Hourglass from "../../images/ampulheta.svg"
 import Wave from "../../images/wave.svg"
 import Ball from "../../images/ball.svg"
-import * as typeformEmbed from "@typeform/embed"
 import * as Styled from "./styled"
 import lang from "../../intl/langs"
-import CloudButton from "../CloudButton"
 
 const HeroLayout = ({ language }) => {
-  const [form, setForm] = useState(null)
-
-  const handleCaledlyAction = () => openPopupWidget({ url: "https://calendly.com/badico" })
-
-  useEffect(() => {
-    const contactForm = typeformEmbed.makePopup(
-      "https://ask767898.typeform.com/to/rRIqbEzo",
-      {
-        mode: "drawer_right",
-        autoClose: 3000,
-        hideHeaders: true,
-        hideFooters: true,
-        onSubmit: function () {
-          console.log("Typeform sucessfully submitted")
-        },
-      }
-    )
-    setForm(contactForm)
-  }, [])
-
   return (
     <Styled.HeroWrapper>
       <Wave className="wave" />
@@ -39,17 +16,8 @@ const HeroLayout = ({ language }) => {
           <Styled.Title>{lang[language]["hero.info.h1"]}</Styled.Title>
           <Styled.Text>{lang[language]["hero.info.p"]}</Styled.Text>
           <Styled.Buttons>
-
-            {/* <CloudButton action={() => form.open()} type="secondary">
-              {lang[language]["hero.buttons.0"]}
-            </CloudButton> */}
-
-            <Typeform language={language}/>
-
-            <CloudButton action={handleCaledlyAction} type="primary">
-              {lang[language]["hero.buttons.1"]}
-            </CloudButton>
-            
+            <TypeformButton language={language}/>
+            <CalendlyButton language={language} type="primary"/>
           </Styled.Buttons>
         </Styled.Info>
         <Styled.BoxImage>
