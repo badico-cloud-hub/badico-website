40fe961ad0a9fbfb87ed8268ff0bcfd54eebde41
ydrouulis <lombardi.droulis@gmail.com>
2021-06-24 11:37:40 -0300

    refac(components/typeform): refac on typeform

--- src/components/Typeform/index.jsx
+++ src/components/Typeform/index.jsx
@@ -1,11 +1,43 @@
-import React, { Component } from "react"
+import React, { Component, useState, useEffect } from "react"
 import * as typeformEmbed from "@typeform/embed"
 // import TypeformButton from "./TypeformButton"
 // import Email from "../../images/email.svg"
+import CloudButton from '../CloudButton'
+import lang from "../../intl/langs"
 
-class Typeform extends Component {
-  componentDidMount() {
-    const popup1 = typeformEmbed.makePopup(
+
+// class Typeform extends Component {
+
+
+//   componentDidMount() {
+//     const popup1 = typeformEmbed.makePopup(
+//       "https://ask767898.typeform.com/to/rRIqbEzo",
+//       {
+//         mode: "drawer_right",
+//         autoClose: 3000,
+//         hideHeaders: true,
+//         hideFooters: true,
+//         onSubmit: function () {
+//           console.log("Typeform sucessfully submitted")
+//         },
+//       }
+//     )
+//     document.getElementById("bt-popup").addEventListener("click", function () {
+//       popup1.open()
+//     })
+//   }
+//   render() {
+//     return <div id="bt-popup">Contact us</div>
+//   }
+// }
+
+// export default Typeform
+
+const Typeform = ({ language }) => {
+  const [form, setForm] = useState(null)
+
+  useEffect(() => {
+    const contactForm = typeformEmbed.makePopup(
       "https://ask767898.typeform.com/to/rRIqbEzo",
       {
         mode: "drawer_right",
@@ -17,13 +49,14 @@ class Typeform extends Component {
         },
       }
     )
-    document.getElementById("bt-popup").addEventListener("click", function () {
-      popup1.open()
-    })
-  }
-  render() {
-    return <div id="bt-popup">Contact us</div>
-  }
+    setForm(contactForm)
+  }, [])
+
+  return(
+    <CloudButton action={() => form.open()}>
+      {lang[language]["hero.buttons.0"]}
+    </CloudButton>
+  )
 }
 
-export default Typeform
+export default Typeform;
